c841be97cbed13d64bec81b218cefe1f7988ee29
Adam Shaw <arshaw@arshaw.com>
2011-02-19 15:48:17 -0800

    upgraded to jquery 1.5

--- src/_loader.js
+++ src/_loader.js
@@ -1,6 +1,6 @@
 (function() {
 
-var JQUERY = 'jquery-1.4.4.min.js';
+var JQUERY = 'jquery-1.5.min.js';
 var JQUERY_UI = 'jquery-ui-1.8.9.custom.min.js';
 var JQUERY_LEGACY = 'jquery-1.3.2.min.js';
 var JQUERY_UI_LEGACY = 'jquery-ui-1.7.3.custom.min.js';
