d39e432ebdfecb53935ba3daf3195fe8c02a17a0
JonathanHuot <jonathan.huot@gmail.com>
2011-05-31 06:18:14 -0700

    Included native support of every option update. See issue 293.

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,4 +1,3 @@
-
  
 function Calendar(element, options, eventSources) {
 	var t = this;
@@ -451,9 +450,13 @@ function Calendar(element, options, eventSources) {
 		if (value === undefined) {
 			return options[name];
 		}
+		options[name] = value;
 		if (name == 'height' || name == 'contentHeight' || name == 'aspectRatio') {
-			options[name] = value;
 			updateSize();
+		}
+		else {
+			$(element).fullCalendar('destroy');
+			$(element).fullCalendar(options);            
 		}
 	}
 	
