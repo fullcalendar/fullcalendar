6f0142dc217e2c10e5757eb20593f1b9a46ab306
Pablo Villalba <pablo@teambox.com>
2011-09-10 17:55:12 +0200

    Adding 4 weeks view

--- src/defaults.js
+++ src/defaults.js
@@ -30,11 +30,13 @@ var defaults = {
 	// time formats
 	titleFormat: {
 		month: 'MMMM yyyy',
+		fourWeeks: "MMM d[ yyyy]{ '&#8212;'[ MMM] d yyyy}",
 		week: "MMM d[ yyyy]{ '&#8212;'[ MMM] d yyyy}",
 		day: 'dddd, MMM d, yyyy'
 	},
 	columnFormat: {
 		month: 'ddd',
+    fourWeeks: "ddd",
 		week: 'ddd M/d',
 		day: 'dddd M/d'
 	},
@@ -57,6 +59,7 @@ var defaults = {
 		today: 'today',
 		month: 'month',
 		week: 'week',
+    fourWeeks: "4 weeks",
 		day: 'day'
 	},
 	
