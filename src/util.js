87d3c14b392e86d447fb897272d2657667495a55
Jonathan Breton <jbreton@kronostechnologies.com>
2012-12-11 11:37:50 -0500

    Slower but safer htmlEscape

--- src/util.js
+++ src/util.js
@@ -254,12 +254,7 @@ function smartProperty(obj, name) { // get a camel-cased/namespaced property of 
 
 
 function htmlEscape(s) {
-	return s.replace(/&/g, '&amp;')
-		.replace(/</g, '&lt;')
-		.replace(/>/g, '&gt;')
-		.replace(/'/g, '&#039;')
-		.replace(/"/g, '&quot;')
-		.replace(/\n/g, '<br />');
+	return $('<div></div>').text(s).html();
 }
 
 
