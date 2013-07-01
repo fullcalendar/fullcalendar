917cdc44cf6e51426db484babd639dc4166f7e13
lukasz-schab <Lukasz@schab.czest.pl>
2013-07-01 15:42:12 +0200

    Update EventManager.js
    backgroudColor To backgroundColor

--- src/EventManager.js
+++ src/EventManager.js
@@ -1,4 +1,3 @@
-
 fc.sourceNormalizers = [];
 fc.sourceFetchers = [];
 
@@ -244,7 +243,7 @@ function EventManager(options, _sources) {
 				e.className = event.className;
 				e.editable = event.editable;
 				e.color = event.color;
-				e.backgroudColor = event.backgroudColor;
+				e.backgroundColor = event.backgroundColor;
 				e.borderColor = event.borderColor;
 				e.textColor = event.textColor;
 				normalizeEvent(e);
