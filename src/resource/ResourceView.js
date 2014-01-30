70aaa79a80e273085f69999c17f00e38498334f4
Daxx909 <Daxx909@users.noreply.github.com>
2014-01-30 17:04:59 +0100

    Use .data for dayClick
    Get data using data-date attribute

--- src/resource/ResourceView.js
+++ src/resource/ResourceView.js
@@ -1,4 +1,3 @@
-
 setDefaults({
 	weekMode: 'fixed',
 	titleFormat: {
@@ -420,8 +419,7 @@ function ResourceView(element, calendar, viewName) {
 	
 	function dayClick(ev) {
 		if (!opt('selectable')) { // if selectable, SelectionManager will worry about dayClick
-			var index = parseInt(this.className.match(/fc\-day(\d+)/)[1]); // TODO: maybe use .data
-			var date = indexDate(index);
+			var date = new Date(Date.parse($(this).data("date")));
 			trigger('dayClick', this, date, true, ev);
 		}
 	}
