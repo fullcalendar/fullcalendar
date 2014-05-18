f359a716ce8af2419f9de420b83577dfcd4d4f75
David Asabina <david@supr.nu>
2014-05-19 00:45:05 +0200

    Cleaned up my test (removed whitespaces on blank lines + trailing whitespaces)

--- tests/automated/buttonText.js
+++ tests/automated/buttonText.js
@@ -71,25 +71,25 @@ describe('button text', function() {
       beforeEach(function() {
         $('#cal').fullCalendar(settings);
       });
-  
+
       it('should contain the default text values', function() {
         expect($('.fc-button-today')).toHaveText('today');
-  
+
         expect($('.fc-button-next')).toHaveText('next');
         expect($('.fc-button-nextYear')).toHaveText('next year');
         expect($('.fc-button-prev')).toHaveText('prev');
         expect($('.fc-button-prevYear')).toHaveText('prev year');
-  
+
         expect($('.fc-button-month')).toHaveText('month');
-  
+
         expect($('.fc-button-basicWeek')).toHaveText('week');
         expect($('.fc-button-agendaWeek')).toHaveText('week');
-  
+
         expect($('.fc-button-basicDay')).toHaveText('day');
         expect($('.fc-button-agendaDay')).toHaveText('day');
       });
     });
-  
+
     describe('when buttonText is specified', function() {
       beforeEach(function() {
         settings.buttonText = {
@@ -101,43 +101,43 @@ describe('button text', function() {
           month: 'mun',
           week: 'wiki',
           day: 'dei'
-        }
+        };
         $('#cal').fullCalendar(settings);
       });
-      
+
       it('should contain the specified text values', function() {
         expect($('.fc-button-today')).toHaveText('tidei');
-  
+
         expect($('.fc-button-next')).toHaveText('->');
         expect($('.fc-button-nextYear')).toHaveText('-->');
         expect($('.fc-button-prev')).toHaveText('<-');
         expect($('.fc-button-prevYear')).toHaveText('<--');
-  
+
         expect($('.fc-button-month')).toHaveText('mun');
-  
+
         expect($('.fc-button-agendaDay')).toHaveText('dei');
         expect($('.fc-button-agendaWeek')).toHaveText('wiki');
-  
+
         expect($('.fc-button-basicDay')).toHaveText('dei');
         expect($('.fc-button-basicWeek')).toHaveText('wiki');
       });
     });
-  
+
     describe('when lang is not default', function() {
-  
+
       beforeEach(function() {
         settings.lang = 'nl';
         $('#cal').fullCalendar(settings);
       });
-   
+
       it('should contain the default text values', function() {
         expect($('.fc-button-today')).toHaveText('Vandaag');
-  
+
         expect($('.fc-button-month')).toHaveText('Maand');
-  
+
         expect($('.fc-button-basicWeek')).toHaveText('Week');
         expect($('.fc-button-agendaWeek')).toHaveText('Week');
-  
+
         expect($('.fc-button-basicDay')).toHaveText('Dag');
         expect($('.fc-button-agendaDay')).toHaveText('Dag');
       });
