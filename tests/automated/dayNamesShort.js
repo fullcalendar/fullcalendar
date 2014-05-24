ccde440850d783ae795950f4c98c88cbddbeac8b
David Asabina <david@supr.nu>
2014-05-24 03:13:46 +0200

    Fixing style issues

--- tests/automated/dayNamesShort.js
+++ tests/automated/dayNamesShort.js
@@ -4,7 +4,7 @@ describe('short day names', function() {
     'month',
     'agendaWeek',
     'basicWeek',
-  ]
+  ];
   var dayClasses = [
     '.fc-sun',
     '.fc-mon',
@@ -18,8 +18,7 @@ describe('short day names', function() {
 
   beforeEach(function() {
     affix('#cal');
-    settings = {
-    }
+    settings = { };
   });
 
   testableClasses.forEach(function(viewClass, index, viewClasses) {
@@ -27,44 +26,44 @@ describe('short day names', function() {
       beforeEach(function() {
         settings.defaultView = viewClass;
       });
-  
+
       describe('when lang is default', function() {
         it('should be in English', function() {
           moment.lang('en');
           $('#cal').fullCalendar(settings);
           var weekdays = moment.weekdaysShort();
-  
+
           dayClasses.forEach(function(cls, index, classes) {
             expect($('.fc-view thead ' + cls)[0]).toContainText(weekdays[index]);
           });
         });
       });
-    
+
       describe('when lang is not default', function() {
         languages.forEach(function(language, index, languages) {
           it('should be in the selected language', function() {
             settings.lang = language;
             $('#cal').fullCalendar(settings);
-    
+
             moment.lang(language);
-            var dow = moment.langData(language)._week.dow
+            var dow = moment.langData(language)._week.dow;
             var weekdays = moment.weekdaysShort();
-    
+
             dayClasses.forEach(function(cls, index, classes) {
               expect($('.fc-view thead ' + cls)[0]).toContainText(weekdays[index]);
             });
           });
         });
       });
-    
+
       describe('when specified', function() {
         it('should contain the specified names in the given order', function() {
           var days = [
-            'Hovjaj', 'maSjaj', 'veSjaj', 'mechjaj', 'parmaqjaj', 'HoSjaj'
-          ]
+            'Hov.', 'maS.', 'veS.', 'mech.', 'parmaq.', 'HoS.'
+          ];
           settings.dayNamesShort = days;
           $('#cal').fullCalendar(settings);
-  
+
           dayClasses.forEach(function(cls, index, classes) {
             expect($('.fc-view thead ' + cls)[0]).toContainText(days[index]);
           });
