82f8192503b6313f796e3af9ae003b7c9f221f1f
Adam Shaw <arshaw@arshaw.com>
2014-05-24 16:17:41 -0700

    formatRange test. notes for testing lang

---
+++ tests/automated/lang.js
@@ -0,0 +1,29 @@
+
+describe('lang', function() {
+
+	/*
+
+	is not affected by global moment lang
+
+	doesn't affect the global moment lang when customized
+
+	defaults to english when configured to language that isn't loaded
+
+
+	### below should be tested for in specific setting files
+
+	is affected by the language's isRTL
+	uses the isRTL setting instead of the language's isRTL
+
+	is affected by the language's firstDay
+	uses the firstDay setting instead of the language's firstDay
+
+	computes the default columnFormat based on the language
+	uses the columnFormat setting instead of the language's columnFormat default
+
+	computes the default timeFormat based on the language
+	uses the timeFormat setting instead of the language's timeFormat default
+
+	*/
+
+});
