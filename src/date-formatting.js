46b401c0907d4ee5bd5c2ecf5bfe4a2796b59ab6
Adam Shaw <arshaw@arshaw.com>
2014-05-24 17:09:46 -0700

    fix bug getting confused with momentjs formatting char LT and our T

--- src/date-formatting.js
+++ src/date-formatting.js
@@ -203,7 +203,7 @@ function getFormatStringChunks(formatStr) {
 // Break the formatting string into an array of chunks
 function chunkFormatString(formatStr) {
 	var chunks = [];
-	var chunker = /\[([^\]]*)\]|\(([^\)]*)\)|((\w)\4*o?T?)|([^\w\[\(]+)/g; // TODO: more descrimination
+	var chunker = /\[([^\]]*)\]|\(([^\)]*)\)|(LT|(\w)\4*o?)|([^\w\[\(]+)/g; // TODO: more descrimination
 	var match;
 
 	while ((match = chunker.exec(formatStr))) {
