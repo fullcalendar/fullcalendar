b20a1275feef0cfa5e942c59e96ed8aae49db9d6
Adam Shaw <arshaw@arshaw.com>
2014-06-01 15:56:14 -0700

    automated tests for axisFormat

---
+++ tests/automated/axisFormat.js
@@ -0,0 +1,33 @@
+describe('axisFormat', function() {
+
+	var options;
+
+	function getAxisText() {
+		return $('.fc-agenda-slots tr th.fc-agenda-axis:first').text();
+	}
+
+	beforeEach(function() {
+		affix('#cal');
+		options = {
+			defaultDate: '2014-06-04',
+			defaultView: 'agendaWeek'
+		};
+	});
+
+	it('renders correctly when default', function() {
+		$('#cal').fullCalendar(options);
+		expect(getAxisText()).toBe('12am');
+	});
+
+	it('renders correctly when default and the language is customized', function() {
+		options.lang = 'en-gb';
+		$('#cal').fullCalendar(options);
+		expect(getAxisText()).toBe('00');
+	});
+
+	it('renders correctly when customized', function() {
+		options.axisFormat = 'H:mm:mm[!]';
+		$('#cal').fullCalendar(options);
+		expect(getAxisText()).toBe('0:00:00!');
+	});
+});
