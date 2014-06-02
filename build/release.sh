484433b07fdc9bd17664d34026d65dd65ab7d5dd
Adam Shaw <arshaw@arshaw.com>
2014-06-02 15:06:46 -0700

    better the assume-unchanged hack

--- build/release.sh
+++ build/release.sh
@@ -11,6 +11,7 @@ fi
 
 grunt bump --setversion=$version && \
 grunt dist && \
+npm run-script no-assume-unchanged && \
 git add *.json && \
 git add -f dist/*.js dist/*.css dist/lang/*.js && \
 git commit -e -m "version $version" && \
@@ -18,3 +19,7 @@ git tag -a v$version -m "version $version" && \
 echo && \
 echo 'DONE. It is now up to you to run `'"git push origin master && git push origin v$version"'`' && \
 echo
+
+# regardless of error/success, undo the temporary no-assume-unchanged
+git reset
+npm run-script assume-unchanged
