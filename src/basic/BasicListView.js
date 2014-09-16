b0fd11000adca6268c88e696a0031053bfc54584
samedi <samedi@samedis-Mac.local>
2014-09-17 01:57:33 +0200

    List finally kind of working

--- src/basic/BasicListView.js
+++ src/basic/BasicListView.js
@@ -36,7 +36,17 @@ $.extend(BasicListView.prototype, {
             ' \u2014 ' // emphasized dash
         );
 
-        BasicView.prototype.render.call(this, 1, 1, false); // call the super-method
+        //this.el.addClass('fc-basic-view').html(this.renderHtml());
+
+        this.scrollerEl = this.el.find('.fc-day-grid-container');
+
+        this.trigger('viewRender', this, this, this.el);
+
+        // attach handlers to document. do it here to allow for destroy/rerender
+        $(document)
+            .on('mousedown', this.documentMousedownProxy)
+            .on('dragstart', this.documentDragStartProxy); // jqui drag
+
     },
 
     renderEvents: function renderBasicListEvents(events) {
@@ -64,9 +74,11 @@ $.extend(BasicListView.prototype, {
 
         }
 
-        var html = '\
-        	<table>\
-        		<tbody>';
+        var segs = [];
+
+        var table = $('<table></table>');
+        var tbody = $('<tbody></tbody>');
+        table.append(tbody);
 
         var current = this.start.clone().startOf('day');
         var end = this.end.clone();
@@ -74,41 +86,87 @@ $.extend(BasicListView.prototype, {
 
             if (current in daysToDisplay) {
 
-                html += '\
+                //Day header
+                tbody.append('\
                 	<tr>\
                 		<th colspan="4">\
                 			<span class="fc-header-date">' + current.format() + '</span>\
                 			<span class="fc-header-day">' + current.format() + '</span>\
                     	</th>\
-                    </tr>';
+                    </tr>');
 
                 var events = daysToDisplay[current];
-                for (var i = 0; i < events.length; ++i)
-                    html += '\
-                		<tr>\
+                for (var i = 0; i < events.length; ++i) {
+                    var e = events[i];
+
+                    var segEl = $('\
+                		<tr class="fc-row fc-event-container fc-content">\
                 			<td class="fc-event-handle">\
                 				<span class="fc-event"></span>\
                 			</td>\
-                			<td class="fc-event-time">' + 'TODO time' + '</td>\
-                			<td class="fc-event-title">' + events[i].title + '</td>\
-                			<td class="fc-event-location">' + events[i].location || '' + '</td>\
-                		</tr>';
+                			<td class="fc-time">' + 'TODO time' + '</td>\
+                			<td class="fc-title">' + e.title + '</td>\
+                			<td class="fc-location">' + e.location || '' + '</td>\
+                		</tr>');
+                    tbody.append(segEl);
+                    
+                    var seg = {
+                        'el': segEl,
+                        'event': e
+                    };
+
+                    //Tried to use fullcalendar code for this stuff but to no avail
+                    var _this = this;
+                    segEl.on('click', function(ev) {
+                        return _this.trigger('eventClick', segEl, e, ev);
+                    });
 
 
+                    segs.push(seg);
+                }
 
             }
 
             current.add('days', 1)
         }
 
-        html += '\
-        		</tbody>\
-        	</table>';
 
-        this.el.html(html);
+        this.el.html(table);
 
-        //View.prototype.renderEvents.call(this, events);
+        this.segs = segs; //used in call below
+        View.prototype.renderEvents.call(this, events);
 
+    },
+
+    updateWidth: function() {
+        // subclasses should implement
+    },
+
+    setHeight: function(height, isAuto) {
+        // subclasses should implement
+    },
+
+    getSegs: function() {
+        return this.segs || [];
+    },
+
+    renderDrag: function(start, end, seg) {
+        // subclasses should implement
+    },
+
+    // Unrenders a visual indication of event hovering
+    destroyDrag: function() {
+        // subclasses should implement
+    },
+
+    // Renders a visual indication of the selection
+    renderSelection: function(start, end) {
+        // subclasses should implement
+    },
+
+    // Unrenders a visual indication of selection
+    destroySelection: function() {
+        // subclasses should implement
     }
 
 });
