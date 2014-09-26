a436315bdc930d9feb8d1cf2adf118833a33ee81
fengyu <fengyu.xy@alibaba-inc.com>
2014-09-26 11:23:37 +0800

    change the code style

--- dist/fullcalendar.js
+++ dist/fullcalendar.js
@@ -5162,12 +5162,12 @@ $.extend(DayGrid.prototype, {
 
 		// Only display a timed events time if it is the starting segment
 		if (!event.allDay && seg.isStart) {
-			timeHtml = '<span class="fc-time">' +'title="'+event.title+'">' + htmlEscape(view.getEventTimeText(event)) + '</span>';
+			timeHtml = '<span class="fc-time">' + 'title="'+event.title+'">' + htmlEscape(view.getEventTimeText(event)) + '</span>';
 		}
 
 		titleHtml =
-			'<span class="fc-title">' +'title="'+event.title+'">' +
-				(htmlEscape(event.title || '') || '&nbsp;') + // we always want one line of height
+			'<span class="fc-title">' + 'title="'+event.title+'">' +
+			 (htmlEscape(event.title || '') || '&nbsp;') + // we always want one line of height
 			'</span>';
 		
 		return '<a class="' + classes.join(' ') + '"' +
