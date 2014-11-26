17b44fbc99570a54382a7b761fef3de1c9620ad6
zvictor <zvictor@zvictor.net>
2014-11-26 15:35:34 +0100

    renders only the relevant events

--- src/Calendar.js
+++ src/Calendar.js
@@ -495,7 +495,9 @@ function Calendar(element, instanceOptions) {
 		if (elementVisible()) {
 			freezeContentHeight();
 			currentView.destroyEvents(); // no performance cost if never rendered
-			currentView.renderEvents(events);
+			currentView.renderEvents($.grep(events, function(event) {
+				return event.end > currentView.start && event.start < currentView.end;
+			}));
 			unfreezeContentHeight();
 		}
 	}
