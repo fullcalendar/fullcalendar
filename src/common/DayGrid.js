87e699e5b5dcf8da362a5cd236ad8b7e2cd0a535
Adam Shaw <arshaw@arshaw.com>
2015-11-14 23:01:13 -0800

    make mixins more convenient

--- src/common/DayGrid.js
+++ src/common/DayGrid.js
@@ -2,7 +2,7 @@
 /* A component that renders a grid of whole-days that runs horizontally. There can be multiple rows, one per week.
 ----------------------------------------------------------------------------------------------------------------------*/
 
-var DayGrid = FC.DayGrid = Grid.extend($.extend({}, DayTableMixin, {
+var DayGrid = FC.DayGrid = Grid.extend(DayTableMixin, {
 
 	numbersVisible: false, // should render a row for day/week numbers? set by outside view. TODO: make internal
 	bottomCoordPadding: 0, // hack for extending the hit area for the last row of the coordinate grid
@@ -417,4 +417,4 @@ var DayGrid = FC.DayGrid = Grid.extend($.extend({}, DayTableMixin, {
 		return skeletonEl;
 	}
 
-}));
+});
