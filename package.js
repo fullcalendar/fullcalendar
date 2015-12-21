00770654b1b2713053bc14f295edf3ddbe8a8e34
Roman <rgnevashev@gmail.com>
2015-12-21 20:35:36 +0300

    Create package.js

---
+++ package.js
@@ -0,0 +1,15 @@
+Package.describe({
+  name: 'fullcalendar:fullcalendar',
+  version: '2.5.0',
+  summary: 'Full-sized drag & drop event calendar',
+  git: 'https://github.com/fullcalendar/fullcalendar.git',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.1');
+  api.addFiles([
+    "dist/fullcalendar.js",
+    "dist/fullcalendar.css"
+  ], 'client');
+}); 
