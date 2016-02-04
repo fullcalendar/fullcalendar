e9249968c7ee5f58a622d640c9bf324eb53bdadf
roman <rgnevashev@gmail.com>
2016-02-04 10:15:30 +0300

    update

--- package.js
+++ package.js
@@ -1,15 +1,15 @@
 Package.describe({
   name: 'fullcalendar:fullcalendar',
-  version: '2.5.0',
+  version: '2.6.0',
   summary: 'Full-sized drag & drop event calendar',
   git: 'https://github.com/fullcalendar/fullcalendar.git',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.2.1');
+  //api.versionsFrom('1.2.1');
   api.addFiles([
     "dist/fullcalendar.js",
     "dist/fullcalendar.css"
   ], 'client');
-}); 
+});
