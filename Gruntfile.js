08f7d88f71196650de72aa87e030557e1458de4b
Adam Shaw <arshaw@users.noreply.github.com>
2016-05-01 01:46:13 -0700

    grunt-jscs-checker -> grunt-jscs

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,7 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
-	grunt.loadNpmTasks('grunt-jscs-checker');
+	grunt.loadNpmTasks('grunt-jscs');
 	grunt.loadNpmTasks('grunt-shell');
 	grunt.loadNpmTasks('grunt-karma');
 	grunt.loadNpmTasks('grunt-bump');
