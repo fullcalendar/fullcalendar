d389830d834e6775cc7f711ededb68d22e210237
Casey Holzer <casey17648@gmail.com>
2016-06-14 10:45:14 -0600

    call fetchEventSources from fetchEvents

--- src/EventManager.js
+++ src/EventManager.js
@@ -68,13 +68,7 @@ function EventManager(options) { // assumed to be a calendar
 	function fetchEvents(start, end) {
 		rangeStart = start;
 		rangeEnd = end;
-		cache = [];
-		var fetchID = ++currentFetchID;
-		var len = sources.length;
-		pendingSourceCnt = len;
-		for (var i=0; i<len; i++) {
-			fetchEventSource(sources[i], fetchID);
-		}
+		fetchEventSources(sources, true);
 	}
 
 
