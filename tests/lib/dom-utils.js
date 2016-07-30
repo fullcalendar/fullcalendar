55244de64e4cf0235c48fda847d879407370c1ee
Adam Shaw <arshaw@users.noreply.github.com>
2016-07-30 13:58:57 -0700

    copypasta test utils. tests for multiple defs of businessHours

--- tests/lib/dom-utils.js
+++ tests/lib/dom-utils.js
@@ -50,3 +50,68 @@ function countHandlers(el) {
 function getHandlerHash(el) {
 	return $._data($(el)[0], 'events') || {};
 }
+
+
+/* copied from other proj
+----------------------------------------------------------------------------------------------------------------------*/
+
+function doElsMatchSegs(els, segs, segToRectFunc) {
+	var elRect, found, i, j, k, len, len1, seg, segRect, unmatchedRects;
+	unmatchedRects = getBoundingRects(els);
+	if (unmatchedRects.length !== segs.length) {
+		return false;
+	}
+	for (j = 0, len = segs.length; j < len; j++) {
+		seg = segs[j];
+		segRect = segToRectFunc(seg);
+		found = false;
+		for (i = k = 0, len1 = unmatchedRects.length; k < len1; i = ++k) {
+			elRect = unmatchedRects[i];
+			if (isRectsSimilar(elRect, segRect)) {
+				unmatchedRects.splice(i, 1);
+				found = true;
+				break;
+			}
+		}
+		if (!found) {
+			return false;
+		}
+	}
+	return true;
+}
+
+function getBoundingRects(els) {
+	var node;
+	return (function() {
+		var i, len, results;
+		results = [];
+		for (i = 0, len = els.length; i < len; i++) {
+			node = els[i];
+			results.push(getBoundingRect(node));
+		}
+		return results;
+	})();
+}
+
+function getBoundingRect(el) {
+	var rect;
+	el = $(el);
+	expect(el.length).toBe(1);
+	rect = el.offset();
+	rect.right = rect.left + el.outerWidth();
+	rect.bottom = rect.top + el.outerHeight();
+	rect.node = el[0];
+	return rect;
+}
+
+function isRectsSimilar(rect1, rect2) {
+	return isRectsHSimilar(rect1, rect2) && isRectsVSimilar(rect1, rect2);
+}
+
+function isRectsHSimilar(rect1, rect2) {
+	return Math.abs(rect1.left - rect2.left) <= 1.1 && Math.abs(rect1.right - rect2.right) <= 1.1;
+}
+
+function isRectsVSimilar(rect1, rect2) {
+	return Math.abs(rect1.top - rect2.top) <= 1.1 && Math.abs(rect1.bottom - rect2.bottom) <= 1.1;
+}
