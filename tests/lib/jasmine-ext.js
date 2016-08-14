e86ab31d5e353d578359e0944a7b653938fbf70c
Adam Shaw <arshaw@users.noreply.github.com>
2016-08-14 16:03:18 -0700

    no need to supress moment warnings

--- tests/lib/jasmine-ext.js
+++ tests/lib/jasmine-ext.js
@@ -4,12 +4,6 @@ beforeEach(function() {
 	// HACK. PhantomJS fails otherwise
 	$.fullCalendar.Calendar.defaults.windowResizeDelay = 200;
 
-	// don't show warnings about deprecated methods like `moment.lang`, etc.
-	// at some point we'll require moment version above 2.8.
-	// until then, it's too annoying to support two versions.
-	// (not the best place for this)
-	moment.suppressDeprecationWarnings = true;
-
 	// increase the default timeout
 	jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
 
