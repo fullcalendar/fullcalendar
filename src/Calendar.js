1e68580cca326c6124664d4c973540fa418384cf
Adam Shaw <arshaw@users.noreply.github.com>
2016-08-14 12:28:18 -0700

    buttonTextKey

--- src/Calendar.js
+++ src/Calendar.js
@@ -184,6 +184,9 @@ var Calendar = FC.Calendar = Class.extend({
 		function queryButtonText(options) {
 			var buttonText = options.buttonText || {};
 			return buttonText[requestedViewType] ||
+				// view can decide to look up a certain key
+				(spec.buttonTextKey ? buttonText[spec.buttonTextKey] : null) ||
+				// a key like "month"
 				(spec.singleUnit ? buttonText[spec.singleUnit] : null);
 		}
 
