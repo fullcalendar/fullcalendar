95d5c909c670c55282b081bbc29667b355f576d2
Adam Shaw <arshaw@users.noreply.github.com>
2016-10-01 12:05:35 -0700

    build system cleanup. consistent with scheduler

--- tasks/minify.js
+++ tasks/minify.js
@@ -16,21 +16,23 @@ gulp.task('minify:clean', function() {
 // minifies the core modules's js
 gulp.task('minify:js', [ 'modules' ], function() {
 	return gulp.src([
-			'dist/fullcalendar.js'
-		])
-		.pipe(uglify({
-			preserveComments: 'some' // keep comments starting with !
-		}))
-		.pipe(rename({ extname: '.min.js' }))
-		.pipe(gulp.dest('dist/'));
+		'dist/*.js',
+		'!dist/*.min.js' // avoid double minify
+	])
+	.pipe(uglify({
+		preserveComments: 'some' // keep comments starting with !
+	}))
+	.pipe(rename({ extname: '.min.js' }))
+	.pipe(gulp.dest('dist/'));
 });
 
 // minifies the core modules's css
 gulp.task('minify:css', [ 'modules' ], function() {
 	return gulp.src([
-			'dist/fullcalendar.css'
-		])
-		.pipe(cssmin())
-		.pipe(rename({ extname: '.min.css' }))
-		.pipe(gulp.dest('dist/'));
+		'dist/*.css',
+		'!dist/*.min.css' // avoid double minify
+	])
+	.pipe(cssmin())
+	.pipe(rename({ extname: '.min.css' }))
+	.pipe(gulp.dest('dist/'));
 });
