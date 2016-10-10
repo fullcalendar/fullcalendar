4cdf6f2fdecd8f4620d1ed6cde7ea0238d5789b4
Axel Duch <axel.duch@teamwork.com>
2016-10-10 12:57:31 +0100

    Remove method Iterator.prototype.iterate and use Array.prototype.forEach on items property instead

--- src/common/Iterator.js
+++ src/common/Iterator.js
@@ -3,17 +3,12 @@ function Iterator(items) {
 }
 
 
-Iterator.prototype.iterate = function(callback) {
-    this.items.forEach(callback);
-};
-
-
 /* Calls a method on every item passing the arguments through */
 Iterator.prototype.proxyCall = function(methodName) {
     var args = Array.prototype.slice.call(arguments, 1);
     var results = [];
 
-    this.iterate(function(item) {
+    this.items.forEach(function(item) {
         results.push(item[methodName].apply(item, args));
     });
 
