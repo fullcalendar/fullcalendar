6c52babf48c0fb2a2a3d5cf729965885821a9c25
Adam Shaw <arshaw@users.noreply.github.com>
2016-10-30 18:15:31 -0700

    clean up test

--- tests/automated/weekViewRender.js
+++ tests/automated/weekViewRender.js
@@ -1,31 +1,35 @@
-describe('weekViewRender', function() {
+fdescribe('weekViewRender', function() {
 
 	beforeEach(function() {
 		affix('#cal');
 	});
 
 	describe('verify th class for today', function() {
-
-		var dataNow = $.fullCalendar.moment(new Date()).format('YYYY-MM-DD');
-
-		var weekView = { view: 'agendaWeek', expectedToday: 'fc-today', selector: 'th.fc-day-header' };
+		var nowStr = $.fullCalendar.moment(new Date()).format('YYYY-MM-DD');
 
 		beforeEach(function() {
 			$('#cal').fullCalendar({
-				defaultDate: dataNow
+				defaultDate: nowStr,
+				defaultView: 'agendaWeek'
 			});
 		});
 
 		it('should have fc-today class only on "today"', function() {
-			var cal = $('#cal');
-			cal.fullCalendar('changeView', weekView.view);
-			var allHeaders = cal.find(weekView.selector);
-			for (var i = 0; i < allHeaders.length; ++i) {
-				var jQueryHeader = $(allHeaders[i]);
-				var today = jQueryHeader.data('date') == dataNow;
-				var hasTodayClass = jQueryHeader.hasClass('fc-today');
-				expect(today).toBe(hasTodayClass);
-			}
+			var foundToday = false;
+
+			$('#cal th.fc-day-header').each(function(i, headerNode) {
+				var headerEl = $(headerNode);
+				var dateMatchesToday = headerEl.data('date') === nowStr;
+				var hasTodayClass = headerEl.hasClass('fc-today');
+
+				expect(dateMatchesToday).toBe(hasTodayClass);
+
+				if (hasTodayClass) {
+					foundToday = true;
+				}
+			});
+
+			expect(foundToday).toBe(true);
 		});
 	});
 });
