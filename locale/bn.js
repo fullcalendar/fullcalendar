fed3faf41f586c0b87c122bb3abd7205a8019752
Sekander Badsha <badsha_eee@yahoo.com>
2016-11-13 11:49:20 +0600

    Added Bengali support
    I am not sure about the `allDayHtml`. So left it as it is. If you can explain, then I will correct it too.

---
+++ locale/bn.js
@@ -0,0 +1,11 @@
+$.fullCalendar.locale("bn", {
+	buttonText: {
+		month: "মাস",
+		week: "সপ্তাহ",
+		day: "দিন",
+		list: "লিস্ট"
+	},
+	allDayHtml: "Todo<br/>el día",
+	eventLimitText: "আরও দেখুন",
+	noEventsMessage: "কোন ইভেন্ট পাওয়া যায়নি"
+});
