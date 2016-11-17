1dd788dafd60f8dea757b599c21ae52dd159bf5a
Horken <horkenwong@gmail.com>
2016-11-17 17:27:30 +0800

    change space to tab
    I make some explain about my change.
    I was change some translate because the original translation was use simplified Chinese, so I just change it to traditional Chinese.
    and some words were wrong used just according to location where we live, so I change made it more suitable

--- locale/zh-tw.js
+++ locale/zh-tw.js
@@ -1,14 +1,12 @@
 
 $.fullCalendar.locale("zh-tw", {
 	buttonText: {
-        month: "月",
-        week: "週",
-        day: "天",
-        list: "活動列表"
-    },
-    allDayText: "整天",
-    eventLimitText: function(n) {
-        return '顯示更多';
-    },
-    noEventsMessage: "没有任何活動"
+		month: "月",
+		week: "週",
+		day: "天",
+		list: "活動列表"
+	},
+	allDayText: "整天",
+	eventLimitText: '顯示更多',
+	noEventsMessage: "没有任何活動"
 });
