eb0703eb8b6dd634a64e182e7d1831a43cce1b00
Adam Shaw <arshaw@users.noreply.github.com>
2017-02-13 13:49:31 -0800

    help tests deal with global mouse ignore

--- tests/lib/jasmine-ext.js
+++ tests/lib/jasmine-ext.js
@@ -4,6 +4,11 @@ beforeEach(function() {
 	// HACK. PhantomJS fails otherwise
 	$.fullCalendar.Calendar.defaults.windowResizeDelay = 200;
 
+	// On real devices, when a click-like touch interaction happens, there is a preiod of time where mouse events
+	// are ignores. Since ignore peroid is global, and might span across tests, disable it.
+	// The simulates touch events do not fire these mouse events anyway.
+	$.fullCalendar.touchMouseIgnoreWait = 0;
+
 	// increase the default timeout
 	jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
 
