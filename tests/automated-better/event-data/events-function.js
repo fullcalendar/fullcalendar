7c1c02777aa818cd2376191125e30f29dac2af5a
Adam Shaw <arshaw@users.noreply.github.com>
2017-03-30 18:32:30 -0400

    test for #3598

--- tests/automated-better/event-data/events-function.js
+++ tests/automated-better/event-data/events-function.js
@@ -53,4 +53,23 @@ describe('events as a function', function() {
 			}
 		});
 	});
+
+	/*it('works with a subsequent gotoDate', function(done) {
+		initCalendar({
+			defaultView: 'month',
+			defaultDate: '2017-03-30',
+			events: function(start, end, timezone, callback) {
+				setTimeout(function() {
+					callback([{
+						title: 'test',
+						start: '2017-03-15'
+					}]);
+					setTimeout(function() {
+						done();
+					}, 1000);
+				}, 1000);
+			}
+		});
+		currentCalendar.gotoDate('2017-03-01');
+	});*/
 });
