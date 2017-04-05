0341c70ec818b292f7ef84088e533d69a133a51f
Ronald Pensa <ronald.pensa@ambientia.ee>
2017-04-05 11:06:59 +0300

    add estonian locale

---
+++ locale/et.js
@@ -0,0 +1,24 @@
+$.fullCalendar.locale("et", {
+	closeText: "Sulge",
+	prevText: "&#xAB;Järgmine",
+	nextText: "Eelmine&#xBB;",
+	currentText: "Täna",
+	monthNames: ["Jaanuar", "Veebruar", "Märts", "Aprill", "Mai", "Juuni", "Juuli", "August", "September", "Oktoober", "November", "Detsember"],
+	monthNamesShort: ["Jaan", "Veeb", "Mär", "Apr", "Mai", "Juun", "Juul", "Aug", "Sep", "Okt", "Nov", "Dets"],
+	dayNamesShort: ["P", "E", "T", "K", "N", "R", "L"],
+	dayNames: ["Pühapäev", "Esmaspäev", "Teisipäev", "Kolmapäev", "Neljapäev", "Reede", "Laupäev"],
+	dayNamesMin: ["P", "E", "T", "K", "N", "R", "L"],
+	weekHeader: "Nd",
+	dateFormat: "d.m.yy",
+	firstDay: 1,
+	buttonText: {
+		today: "Täna",
+		month: "Kuu",
+		week: "Nädal",
+		day: "Päev",
+		list: "Nimekiri"
+	},
+	allDayText: "Terve päev",
+	eventLimitText: "veel",
+	noEventsMessage: "sündmused puuduvad"
+});
