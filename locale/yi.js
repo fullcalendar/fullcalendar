23664a0e5a0ca9787b76e41922367c9848999c73
zvi <5633910@gmail.com>
2017-04-13 16:56:59 +0300

    Create yi.js
    Add Yiddish language

---
+++ locale/yi.js
@@ -0,0 +1,10 @@
+$.fullCalendar.locale("yi", {
+	buttonText: {
+		month: "מאָנאַט",
+		week: "וואָך",
+		day: "טאָג",
+	},
+	allDayText: "אַ גאַנצן טאָג",
+	noEventsMessage: "נישט פאַרהאַן קיין עווענט'ס",
+	weekNumberTitle: "וואָך"
+});
