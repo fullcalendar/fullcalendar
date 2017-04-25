c8c40ac1ead03d5c666c5aa4c648a4841c8627fd
David@T460 <david@shipsfocus.com>
2017-04-26 02:04:05 +0800

    Fix when on event drop, freeze scroll bar

--- src/common/View.js
+++ src/common/View.js
@@ -668,8 +668,10 @@ var View = FC.View = Class.extend(EmitterMixin, ListenerMixin, {
 		if (!(this.capturedScrollDepth++)) {
 			this.capturedScroll = this.isDateRendered ? this.queryScroll() : {}; // require a render first
 
-			if(this.opt('scrollTime') != ''){
-				this.capturedScroll.isComputed = true;
+			if(this.capturedScroll){
+				if(this.opt('scrollTime') != '' && this.captureScroll.left == 0){
+					this.capturedScroll.isComputed = true;
+				}
 			}
 			return true; // root?
 		}
