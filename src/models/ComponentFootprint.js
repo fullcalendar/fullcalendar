c22b8667d8a46f1636405cefd75057e07bbbd27f
Adam Shaw <arshaw@users.noreply.github.com>
2017-05-10 15:01:47 -0400

    allday to ComponentFootprint

--- src/models/ComponentFootprint.js
+++ src/models/ComponentFootprint.js
@@ -2,9 +2,11 @@
 var ComponentFootprint = Class.extend({
 
 	dateRange: null,
+	isAllDay: false,
 
-	constructor: function(dateRange) {
+	constructor: function(dateRange, isAllDay) {
 		this.dateRange = dateRange;
+		this.isAllDay = isAllDay;
 	}
 
 });
