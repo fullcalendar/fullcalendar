49ee5fd03d5d0c1a69cc7dec6003c2d8fbd3db51
Adam Shaw <arshaw@users.noreply.github.com>
2017-07-03 17:13:09 -0400

    test for segment timezone text

---
+++ tests/event-render/timeText.js
@@ -0,0 +1,31 @@
+
+describe('the time text on events', function() {
+
+	describe('in agendaWeek', function() {
+		pushOptions({
+			defaultView: 'agendaWeek',
+			defaultDate: '2017-07-03',
+			scrollTime: '00:00'
+		});
+
+		it('renders segs with correct local timezone', function() {
+			initCalendar({
+				timezone: 'local',
+				timeFormat: 'h:mm Z',
+				events: [
+					{ start: '2017-07-03T23:00:00', end: '2017-07-04T13:00:00' }
+				]
+			});
+
+			expect(
+				TimeGridEventRenderUtils.getTimeTexts()
+			).toEqual([
+				moment('2017-07-03T23:00:00').format('h:mm Z') + ' - ' +
+				moment('2017-07-04T00:00:00').format('h:mm Z'),
+				moment('2017-07-04T00:00:00').format('h:mm Z') + ' - ' +
+				moment('2017-07-04T13:00:00').format('h:mm Z'),
+			]);
+		})
+	});
+
+});
