0d69aca083f5d3e75ad3d47c2d94ea95be380400
Adam Shaw <arshaw@users.noreply.github.com>
2017-07-14 22:54:42 -0400

    test for changing allDay with updateEvent

--- tests/event-data/updateEvent.js
+++ tests/event-data/updateEvent.js
@@ -72,4 +72,45 @@ describe('updateEvent', function() {
 			});
 		});
 	});
+
+	describe('when changing an event from timed to all-day', function() {
+		pushOptions({
+			defaultView: 'month',
+			now: '2017-07-14',
+			events: [
+				{ id: '2', start: '2017-07-14T08:00:00Z', end: '2017-07-14T12:00:00Z' }
+			]
+		});
+
+		it('reflects the change on the event object', function(done) {
+			var allRenderCnt = 0;
+
+			initCalendar({
+				eventAfterAllRender: function() {
+					var eventObj;
+
+					allRenderCnt++;
+
+					if (allRenderCnt === 1) {
+						eventObj = currentCalendar.clientEvents('2')[0];
+
+						expect(eventObj.allDay).toBe(false);
+
+						eventObj.allDay = true;
+						eventObj.start = '2017-07-14';
+						eventObj.end = '2017-07-15';
+						currentCalendar.updateEvent(eventObj);
+
+						eventObj = currentCalendar.clientEvents('2')[0];
+
+						expect(eventObj.allDay).toBe(true);
+						expect(eventObj.start.format()).toBe('2017-07-14');
+						expect(eventObj.end.format()).toBe('2017-07-15');
+
+						done();
+					}
+				}
+			});
+		});
+	});
 });
