973b271fef197923619da6178e482e8fb8bfc38a
Adam Shaw <arshaw@users.noreply.github.com>
2017-08-18 15:08:41 -0400

    improve EventInstanceDataSourceSplitter

--- src/models/EventInstanceDataSourceSplitter.js
+++ src/models/EventInstanceDataSourceSplitter.js
@@ -1,16 +1,27 @@
 
-var EventInstanceDataSourceSplitter = Class.extend(EmitterMixin, ListenerMixin, {
+var EventInstanceDataSourceSplitter = FC.EventInstanceDataSourceSplitter = Class.extend(EmitterMixin, ListenerMixin, {
 
 	keysFunc: null,
+	repoHash: null,
+	isPopulated: false, // hack for eventAfterAllRender
 
 
 	constructor: function(keysFunc) {
 		this.keysFunc = keysFunc;
+		this.repoHash = {};
 	},
 
 
 	buildSubSource: function(key) {
 		var subDataSource = new EventInstanceDataSource();
+		var initialRepo = this.repoHash[key];
+
+		if (initialRepo) {
+			subDataSource.addChangeset(new EventInstanceChangeset(null, initialRepo));
+		}
+		else if (this.isPopulated) {
+			subDataSource.isPopulated = true;
+		}
 
 		this.on('receive:' + key, function(changeset) {
 			subDataSource.addChangeset(changeset);
@@ -20,7 +31,7 @@ var EventInstanceDataSourceSplitter = Class.extend(EmitterMixin, ListenerMixin, 
 			if (!subDataSource.isPopulated) {
 				subDataSource.addChangeset(new EventInstanceChangeset());
 			}
-		})
+		});
 
 		return subDataSource;
 	},
@@ -61,10 +72,18 @@ var EventInstanceDataSourceSplitter = Class.extend(EmitterMixin, ListenerMixin, 
 		});
 
 		for (key in changesetsByKey) {
+			changesetsByKey[key].applyToRepo(this.ensureRepo(key));
+
 			this.trigger('receive:' + key, changesetsByKey[key]);
 		}
 
 		this.trigger('after:receive');
+		this.isPopulated = true;
+	},
+
+
+	ensureRepo: function(key) {
+		return (this.repoHash[key] || (this.repoHash[key] = new EventInstanceRepo()));
 	}
 
 });
