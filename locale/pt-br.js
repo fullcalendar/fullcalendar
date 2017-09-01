65782a71e10d54fabf345b535763824edf87df40
Helder Nascimento <heldernascimento.dev@gmail.com>
2017-08-31 23:32:41 -0300

    Update locale pt-br

--- locale/pt-br.js
+++ locale/pt-br.js
@@ -1,14 +1,19 @@
 
 $.fullCalendar.locale("pt-br", {
 	buttonText: {
+		today: "Hoje",
 		month: "Mês",
 		week: "Semana",
 		day: "Dia",
-		list: "Compromissos"
+		list: "Lista"
 	},
 	allDayText: "dia inteiro",
 	eventLimitText: function(n) {
 		return "mais +" + n;
 	},
+	dayNames:['Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sábado'],
+	dayNamesShort: ['Dom','Seg','Ter','Qua','Qui','Sex','Sab'],
+    	monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+    	monthNamesShort: ['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez'],
 	noEventsMessage: "Não há eventos para mostrar"
 });
