35a988d34ce82017e24a8484445656a65ce5728a
Adam Shaw <arshaw@users.noreply.github.com>
2017-09-06 17:28:43 -0400

    up internal version num

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ var FC = $.fullCalendar = {
 	// When introducing internal API incompatibilities (where fullcalendar plugins would break),
 	// the minor version of the calendar should be upped (ex: 2.7.2 -> 2.8.0)
 	// and the below integer should be incremented.
-	internalApiVersion: 9
+	internalApiVersion: 10
 };
 var fcViews = FC.views = {};
 
