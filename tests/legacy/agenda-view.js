3642288ba40aa257fcc1ba649fccd44f59dbc916
Jared Loosli <jloosli@gmail.com>
2017-11-07 10:36:37 -0700

    Updated agenda-view according to latest test style

--- tests/legacy/agenda-view.js
+++ tests/legacy/agenda-view.js
@@ -1,51 +1,43 @@
+describe('agenda view rendering', function () {
 
-describe('agenda view rendering', function() {
-
-	beforeEach(function() {
-		affix('#cal');
-	});
-
-	describe('when isRTL is false', function() {
-
-		beforeEach(function() {
-			$('#cal').fullCalendar({
-				defaultView: 'agendaWeek',
-				isRTL: false
-			});
+	describe('when isRTL is false', function () {
+		pushOptions({
+			defaultView: 'agendaWeek',
+			isRTL: false
 		});
 
-		it('should have have days ordered sun to sat', function() {
-			var fc = $('#cal').find('.fc-view > table > thead th');
-			expect(fc[0]).toHaveClass('fc-axis');
-			expect(fc[1]).toHaveClass('fc-sun');
-			expect(fc[2]).toHaveClass('fc-mon');
-			expect(fc[3]).toHaveClass('fc-tue');
-			expect(fc[4]).toHaveClass('fc-wed');
-			expect(fc[5]).toHaveClass('fc-thu');
-			expect(fc[6]).toHaveClass('fc-fri');
-			expect(fc[7]).toHaveClass('fc-sat');
+		it('should have have days ordered sun to sat', function () {
+			initCalendar();
+			var headers = $('.fc-view > table > thead th');
+			expect(headers[0]).toHaveClass('fc-axis');
+			expect(headers[1]).toHaveClass('fc-sun');
+			expect(headers[2]).toHaveClass('fc-mon');
+			expect(headers[3]).toHaveClass('fc-tue');
+			expect(headers[4]).toHaveClass('fc-wed');
+			expect(headers[5]).toHaveClass('fc-thu');
+			expect(headers[6]).toHaveClass('fc-fri');
+			expect(headers[7]).toHaveClass('fc-sat');
 		});
 	});
 
-	describe('when isRTL is true', function() {
-
-		beforeEach(function() {
-			$('#cal').fullCalendar({
-				defaultView: 'agendaWeek',
-				isRTL: true
-			});
+	describe('when isRTL is true', function () {
+		pushOptions({
+			defaultView: 'agendaWeek',
+			isRTL: true
 		});
 
-		it('should have have days ordered sat to sun', function() {
-			var fc = $('#cal').find('.fc-view > table > thead th');
-			expect(fc[0]).toHaveClass('fc-sat');
-			expect(fc[1]).toHaveClass('fc-fri');
-			expect(fc[2]).toHaveClass('fc-thu');
-			expect(fc[3]).toHaveClass('fc-wed');
-			expect(fc[4]).toHaveClass('fc-tue');
-			expect(fc[5]).toHaveClass('fc-mon');
-			expect(fc[6]).toHaveClass('fc-sun');
-			expect(fc[7]).toHaveClass('fc-axis');
+		it('should have have days ordered sat to sun', function () {
+			initCalendar();
+			var headers = $('.fc-view > table > thead th');
+			// var fc = $('#cal').find('.fc-view > table > thead th');
+			expect(headers[0]).toHaveClass('fc-sat');
+			expect(headers[1]).toHaveClass('fc-fri');
+			expect(headers[2]).toHaveClass('fc-thu');
+			expect(headers[3]).toHaveClass('fc-wed');
+			expect(headers[4]).toHaveClass('fc-tue');
+			expect(headers[5]).toHaveClass('fc-mon');
+			expect(headers[6]).toHaveClass('fc-sun');
+			expect(headers[7]).toHaveClass('fc-axis');
 		});
 	});
 
