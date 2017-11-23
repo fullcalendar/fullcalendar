9d109718770f8fbd0b9feca480a92e9665639ddd
Adam Shaw <arshaw@users.noreply.github.com>
2017-11-23 12:21:08 -0500

    workaround for default export in .d.ts, in build step

--- tasks/ts-types.js
+++ tasks/ts-types.js
@@ -1,15 +1,67 @@
 var gulp = require('gulp');
 var generateDts = require('dts-generator').default;
+var replace = require('gulp-replace');
 
-gulp.task('ts-types', function() {
+gulp.task('ts-types', [ 'ts-types-raw' ], function() {
+	return gulp.src('tmp/fullcalendar.d.ts')
+		.pipe(replace(moduleDeclarationRegex, filterModuleDeclaration))
+		.pipe(gulp.dest('dist/'));
+});
+
+gulp.task('ts-types-raw', function() {
 	return generateDts({
+		project: '.', // where the tsconfig is
 		name: 'fullcalendar',
-		project: '.',
-		out: 'dist/fullcalendar.d.ts',
-		main: 'fullcalendar/src/main'
+		main: 'fullcalendar/src/main',
+		out: 'tmp/fullcalendar.d.ts',
 	});
 });
 
 gulp.task('ts-types:watch', [ 'ts-types' ], function() {
 	return gulp.watch('src/**/*.ts', [ 'ts-types' ]);
 });
+
+
+// Typedef Source Code Transformation Hacks
+// ----------------------------------------
+
+var moduleDeclarationRegex = /^declare module '([^']*)' \{([\S\s]*?)[\n\r]+\}/mg;
+var importFromRegex = /from '([^']*)'/g;
+
+function filterModuleDeclaration(whole, id, body) {
+	return "declare module '" + filterModuleId(id) + "' {" + filterModuleBody(body) + "\n}";
+}
+
+function filterModuleId(id) {
+	// fullcalendar/src/something/MyClass -> fullcalendar/MyClass
+	return id.replace(
+		/\/src\/([^\/]*\/)*([A-Z][A-Za-z]*)$/,
+		'/$2'
+	);
+}
+
+function filterModuleBody(s) {
+	var defaultExportName;
+
+	// changes the name of the default export to `Default` and exports it as a *named* export.
+	// this allows ambient declaration merging to grab onto it.
+	// workaround for https://github.com/Microsoft/TypeScript/issues/14080
+	s = s.replace(/export default( abstract)? class ([A-Z][A-Za-z]*)/, function(m0, m1, m2) {
+		defaultExportName = m2;
+		return 'export' + (m1 || '') + ' class Default';
+	});
+
+	if (defaultExportName) {
+		// replace any references to the original class' name
+		s = s.replace(new RegExp('\\b' + defaultExportName + '\\b'), 'Default');
+
+		// still needs to be exported as default
+		s += "\n\texport default Default;";
+	}
+
+	s = s.replace(importFromRegex, function(whole, id) {
+		return "from '" + filterModuleId(id) + "'";
+	});
+
+	return s;
+}
