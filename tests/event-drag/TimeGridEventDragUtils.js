422d1582e815068e27b3921029be08fde155022a
Adam Shaw <arshaw@users.noreply.github.com>
2017-12-07 15:13:17 -0500

    module TimeGridEventDragUtils

--- tests/event-drag/TimeGridEventDragUtils.js
+++ tests/event-drag/TimeGridEventDragUtils.js
@@ -1,25 +1,21 @@
 
-var TimeGridEventDragUtils = {
+export function drag(startDate, endDate, debug) {
 
-	drag: function(startDate, endDate, debug) {
+	startDate = $.fullCalendar.moment.parseZone(startDate);
+	endDate = $.fullCalendar.moment.parseZone(endDate);
 
-		startDate = $.fullCalendar.moment.parseZone(startDate);
-		endDate = $.fullCalendar.moment.parseZone(endDate);
+	var startRect = TimeGridEventRenderUtils.computeSpanRects(
+		startDate,
+		startDate.clone().add({ minutes: 30 }) // hardcoded 30 minute slot :(
+	)[0];
+	var endRect = TimeGridEventRenderUtils.computeSpanRects(
+		endDate,
+		endDate.clone().add({ minutes: 30 }) // hardcoded 30 minute slot :(
+	)[0];
 
-		var startRect = TimeGridEventRenderUtils.computeSpanRects(
-			startDate,
-			startDate.clone().add({ minutes: 30 }) // hardcoded 30 minute slot :(
-		)[0];
-		var endRect = TimeGridEventRenderUtils.computeSpanRects(
-			endDate,
-			endDate.clone().add({ minutes: 30 }) // hardcoded 30 minute slot :(
-		)[0];
-
-		return EventDragUtils.drag(
-			startRect,
-			endRect,
-			debug
-		);
-	}
-
-};
+	return EventDragUtils.drag(
+		startRect,
+		endRect,
+		debug
+	);
+}
--- tests/event-drag/repeating.js
+++ tests/event-drag/repeating.js
@@ -1,3 +1,4 @@
+import * as TimeGridEventDragUtils from './TimeGridEventDragUtils';
 
 describe('event dragging on repeating events', function() {
 	pushOptions({
