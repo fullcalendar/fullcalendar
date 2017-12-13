1337e2c6c6fa85bd8924bd8ef454e74c32545937
Adam Shaw <arshaw@users.noreply.github.com>
2017-12-13 14:08:04 -0500

    use real extendsStatic in tslib-lite

--- src/tslib-lite.js
+++ src/tslib-lite.js
@@ -7,12 +7,8 @@ only include the helpers we need, to keep down filesize
 */
 
 var extendStatics =
-	/* NOTE: tslib's as-is method is not compatible with how CoffeeScript does subclasses.
-	 * When CoffeeScript is stripped out, can revert.
-	 *
-	 * Object.setPrototypeOf ||
-	 * ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-	 */
+	Object.setPrototypeOf ||
+	({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
 	function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
 
 exports.__extends = function (d, b) {
