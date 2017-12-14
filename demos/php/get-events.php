bf37e17ed8b3ef08111946ea854663630c6bab19
Adam Shaw <arshaw@users.noreply.github.com>
2017-12-14 13:11:06 -0500

    in demos, change to space, fix whitespace

--- demos/php/get-events.php
+++ demos/php/get-events.php
@@ -14,7 +14,7 @@ require dirname(__FILE__) . '/utils.php';
 
 // Short-circuit if the client did not give us a date range.
 if (!isset($_GET['start']) || !isset($_GET['end'])) {
-	die("Please provide a date range.");
+  die("Please provide a date range.");
 }
 
 // Parse the start/end parameters.
@@ -26,7 +26,7 @@ $range_end = parseDateTime($_GET['end']);
 // Parse the timezone parameter if it is present.
 $timezone = null;
 if (isset($_GET['timezone'])) {
-	$timezone = new DateTimeZone($_GET['timezone']);
+  $timezone = new DateTimeZone($_GET['timezone']);
 }
 
 // Read and parse our events JSON file into an array of event data arrays.
@@ -37,13 +37,13 @@ $input_arrays = json_decode($json, true);
 $output_arrays = array();
 foreach ($input_arrays as $array) {
 
-	// Convert the input array into a useful Event object
-	$event = new Event($array, $timezone);
+  // Convert the input array into a useful Event object
+  $event = new Event($array, $timezone);
 
-	// If the event is in-bounds, add it to the output
-	if ($event->isWithinDayRange($range_start, $range_end)) {
-		$output_arrays[] = $event->toArray();
-	}
+  // If the event is in-bounds, add it to the output
+  if ($event->isWithinDayRange($range_start, $range_end)) {
+    $output_arrays[] = $event->toArray();
+  }
 }
 
 // Send JSON to the client.
