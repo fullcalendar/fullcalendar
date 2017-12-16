003ae83b8b9e26d3a1ca8ccb91d6718ade0b496a
Adam Shaw <arshaw@users.noreply.github.com>
2017-12-15 23:44:32 -0500

    better display webpack output

--- tasks/webpack.js
+++ tasks/webpack.js
@@ -1,8 +1,9 @@
 const gulp = require('gulp')
-const webpack = require('webpack-stream')
+const gutil = require('gulp-util')
 const filter = require('gulp-filter')
 const modify = require('gulp-modify-file')
 const uglify = require('gulp-uglify')
+const webpack = require('webpack-stream')
 const packageConfig = require('../package.json')
 const webpackConfig = require('../webpack.config')
 
@@ -30,10 +31,14 @@ function createStream(isDev, isWatch, doneCallback) {
   let doneCallbackCalled = false
   let stream = gulp.src([]) // don't pass in any files. webpack handles that
     .pipe(
-      webpack(Object.assign({}, webpackConfig, {
-        devtool: isDev ? 'source-map' : false, // also 'inline-source-map'
-        watch: isWatch || false
-      }))
+      webpack(
+        Object.assign({}, webpackConfig, {
+          devtool: isDev ? 'source-map' : false, // also 'inline-source-map'
+          watch: isWatch || false
+        }),
+        null,
+        reporterFunc
+      )
     )
     .pipe(
       // don't write bogus .css.js(.map) files webpack created for standalone css outputs
@@ -81,4 +86,45 @@ function createStream(isDev, isWatch, doneCallback) {
       setTimeout(doneCallback, 100) // HACK: for some reason files not written an this point, so wait
     }
   })
+}
+
+/*
+Purpose is to filter out the poorly formatted stats for locale files
+*/
+function reporterFunc(err, stats) {
+  let assets = stats.compilation.assets
+  let filteredAssets = {}
+  let localeModuleCnt = 0
+
+  for (let moduleName in assets) {
+    if (moduleName.match(/^dist\/locale/)) {
+      localeModuleCnt++
+    } else {
+      filteredAssets[moduleName] = assets[moduleName]
+    }
+  }
+
+  stats.compilation.assets = filteredAssets
+  gutil.log(
+    stats.toString({
+      // from https://github.com/shama/webpack-stream/blob/v4.0.0/index.js#L12
+      colors: true,
+      hash: false,
+      timings: false,
+      chunks: false,
+      chunkModules: false,
+      modules: false,
+      children: true,
+      version: true,
+      cached: false,
+      cachedAssets: false,
+      reasons: false,
+      source: false,
+      errorDetails: false
+    })
+  )
+
+  if (localeModuleCnt) {
+    gutil.log(`Also compiled ${localeModuleCnt} locale-related files.`)
+  }
 }
