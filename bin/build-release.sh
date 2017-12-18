1fd03ae8b473cde026fabb393a2bafec7d7ddf10
Adam Shaw <arshaw@users.noreply.github.com>
2017-12-18 00:13:56 -0500

    add .d.ts for release

--- bin/build-release.sh
+++ bin/build-release.sh
@@ -51,7 +51,7 @@ then
   if {
     git checkout --quiet --detach &&
     git add *.json &&
-    git add -f dist/*.js dist/*.css dist/locale/*.js &&
+    git add -f dist/*.js dist/*.d.ts dist/*.css dist/locale/*.js &&
     git commit --quiet --no-verify -e -m "version $version" &&
     git tag -a "v$version" -m "version $version"
   }
