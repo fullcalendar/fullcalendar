a5338bc0520c109864a3b0d0cf61ea213a71915d
acerix <dylan@psilly.com>
2018-01-04 14:02:06 -0500

    Update legacy tests

--- tests/legacy/eventLimit-popoverDestroy.js
+++ tests/legacy/eventLimit-popoverDestroy.js
@@ -1,41 +1,35 @@
-
 describe('eventLimit popover', function() {
 
-  var options
-
-  beforeEach(function() {
-    affix('#cal')
-    options = {
-      defaultView: 'month',
-      defaultDate: '2014-08-01',
-      eventLimit: 3,
-      events: [
-        { title: 'event1', start: '2014-07-28', end: '2014-07-30', className: 'event1' },
-        { title: 'event2', start: '2014-07-29', end: '2014-07-31', className: 'event2' },
-        { title: 'event3', start: '2014-07-29', className: 'event3' },
-        { title: 'event4', start: '2014-07-29', className: 'event4' }
-      ],
-      popoverViewportConstrain: false, // because PhantomJS window is small, don't do smart repositioning
-      handleWindowResize: false // because showing the popover causes scrollbars and fires resize
-    }
+  pushOptions({
+    defaultView: 'month',
+    defaultDate: '2014-08-01',
+    eventLimit: 3,
+    events: [
+      { title: 'event1', start: '2014-07-28', end: '2014-07-30', className: 'event1' },
+      { title: 'event2', start: '2014-07-29', end: '2014-07-31', className: 'event2' },
+      { title: 'event3', start: '2014-07-29', className: 'event3' },
+      { title: 'event4', start: '2014-07-29', className: 'event4' }
+    ],
+    popoverViewportConstrain: false, // because PhantomJS window is small, don't do smart repositioning
+    handleWindowResize: false // because showing the popover causes scrollbars and fires resize
   })
 
   it('closes when user clicks the X and trigger eventDestroy for every render', function() {
     var eventsRendered = {}
     var renderCount = 0
     var activated = false
-    options.eventRender = function(eventObject, element, view) {
-      if (activated) {
-        eventsRendered[eventObject.title] = eventObject
-        ++renderCount
+    initCalendar({
+      eventRender: function(eventObject, element, view) {
+        if (activated) {
+          eventsRendered[eventObject.title] = eventObject
+          ++renderCount
+        }
+      },
+      eventDestroy: function(eventObject, element, view) {
+        delete eventsRendered[eventObject.title]
+        --renderCount
       }
-    }
-    options.eventDestroy = function(eventObject, element, view) {
-      delete eventsRendered[eventObject.title]
-      --renderCount
-    }
-    // Create calendar
-    $('#cal').fullCalendar(options)
+    })
     // Activate flags and pop event limit popover
     activated = true
     $('.fc-more').simulate('click')
@@ -52,18 +46,18 @@ describe('eventLimit popover', function() {
     var eventsRendered = {}
     var renderCount = 0
     var activated = false
-    options.eventRender = function(eventObject, element, view) {
-      if (activated) {
-        eventsRendered[eventObject.title] = eventObject
-        ++renderCount
+    initCalendar({
+      eventRender: function(eventObject, element, view) {
+        if (activated) {
+          eventsRendered[eventObject.title] = eventObject
+          ++renderCount
+        }
+      },
+      eventDestroy: function(eventObject, element, view) {
+        delete eventsRendered[eventObject.title]
+        --renderCount
       }
-    }
-    options.eventDestroy = function(eventObject, element, view) {
-      delete eventsRendered[eventObject.title]
-      --renderCount
-    }
-    // Create calendar
-    $('#cal').fullCalendar(options)
+    })
     // Activate flags and pop event limit popover
     activated = true
     $('.fc-more').simulate('click')
