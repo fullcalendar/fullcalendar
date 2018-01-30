745ac717f19b8e6b3043be7950eec92c071ed76e
Adam Shaw <arshaw@users.noreply.github.com>
2018-01-29 22:30:09 -0500

    need ts-types for examples repo test

--- tasks/example-repos.js
+++ tasks/example-repos.js
@@ -1,6 +1,6 @@
 const gulp = require('gulp')
 const shell = require('gulp-shell')
 
-gulp.task('example-repos:test', [ 'webpack' ], shell.task(
+gulp.task('example-repos:test', [ 'webpack', 'ts-types' ], shell.task(
   './bin/test-typescript-example.sh'
 ))
