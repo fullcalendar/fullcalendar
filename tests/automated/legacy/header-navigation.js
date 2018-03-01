a49d47751a9675cd0679dd5429002b90bdafa2f6
Aakif Nazir <webmaster@nixfifty.com>
2018-03-01 16:19:23 +0000

    port header-navigation test

--- tests/automated/legacy/header-navigation.js
+++ tests/automated/legacy/header-navigation.js
@@ -1,61 +1,61 @@
 
 describe('header navigation', function() {
 
-  beforeEach(function() {
-    affix('#calendar')
-    var options = {
-      header: {
-        left: 'next,prev,prevYear,nextYear today',
-        center: '',
-        right: 'title'
-      }
+  pushOptions({
+    now: '2010-02-01',
+    header: {
+      left: 'next,prev,prevYear,nextYear today',
+      center: '',
+      right: 'title'
     }
-    $('#calendar').fullCalendar(options)
   })
 
   describe('and click next', function() {
-    it('should change view to next month', function() {
-      $('#calendar').fullCalendar('gotoDate', '2010-02-01')
+    it('should change view to next day', function() {
+      initCalendar()
       $('.fc-next-button').simulate('click')
-      var newDate = $('#calendar').fullCalendar('getDate')
+      var newDate = currentCalendar.getDate();
       expect(newDate).toEqualMoment('2010-03-01')
     })
   })
 
   describe('and click prev', function() {
-    it('should change view to prev month', function() {
-      $('#calendar').fullCalendar('gotoDate', '2010-02-01')
+    it('should change view to prev day', function() {
+      initCalendar()
       $('.fc-prev-button').simulate('click')
-      var newDate = $('#calendar').fullCalendar('getDate')
+      var newDate = currentCalendar.getDate();
       expect(newDate).toEqualMoment('2010-01-01')
     })
   })
 
   describe('and click prevYear', function() {
-    it('should change view to prev month', function() {
-      $('#calendar').fullCalendar('gotoDate', '2010-02-01')
+    it('should change view to prev year', function() {
+      initCalendar()
       $('.fc-prevYear-button').simulate('click')
-      var newDate = $('#calendar').fullCalendar('getDate')
-      expect(newDate).toEqualMoment('2009-02-01')
+      var newDate = currentCalendar.getDate();
+      expect(newDate).toEqualMoment('2009-01-01')
     })
   })
 
   describe('and click nextYear', function() {
-    it('should change view to prev month', function() {
-      $('#calendar').fullCalendar('gotoDate', '2010-02-01')
+    it('should change view to next year', function() {
+      initCalendar()
       $('.fc-nextYear-button').simulate('click')
-      var newDate = $('#calendar').fullCalendar('getDate')
+      var newDate = currentCalendar.getDate();
       expect(newDate).toEqualMoment('2011-02-01')
     })
   })
 
   describe('and click today', function() {
-    it('should change view to prev month', function() {
-      $('#calendar').fullCalendar('gotoDate', '2010-02-01')
-      $('.fc-today-button').simulate('click')
-      var newDate = $('#calendar').fullCalendar('getDate') // will be ambig zone
-      newDate.local() // assign the local timezone
-      expect(newDate).toEqualNow()
+    it('should change view to today', function() {
+      var options = {}
+      options.defaultDate = '2010-03-15'; // something other than the `now` date
+
+      initCalendar(options);
+
+      $('.fc-today-button').simulate('click');
+      var newDate = currentCalendar.getDate(); // will be ambig zone
+      expect(newDate).toEqualMoment('2010-02-01')
     })
   })
 })
