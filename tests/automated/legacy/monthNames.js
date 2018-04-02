22719270f60d33d0e856ff8d38fc50f3097e47a8
Dylan <dylan@psilly.com>
2018-04-02 17:02:09 -0400

    Update legacy tests

--- tests/automated/legacy/monthNames.js
+++ tests/automated/legacy/monthNames.js
@@ -1,13 +1,9 @@
 describe('month name', function() {
-  var settings = {}
   var referenceDate = '2014-01-01' // The day the world is hung-over
   var locales = [ 'es', 'fr', 'de', 'zh-cn', 'nl' ]
 
-  beforeEach(function() {
-    affix('#cal')
-    settings = {
-      defaultDate: referenceDate
-    }
+  pushOptions({
+    defaultDate: referenceDate
   })
 
   afterEach(function() {
@@ -16,25 +12,27 @@ describe('month name', function() {
 
   [ 'month', 'agendaDay', 'basicDay' ].forEach(function(viewClass, index, viewClasses) {
     describe('when view is ' + viewClass, function() {
-      beforeEach(function() {
-        settings.defaultView = viewClass
+      pushOptions({
+        defaultView: viewClass
       })
 
       describe('when locale is default', function() {
         beforeEach(function() {
-          settings.locale = 'en'
           moment.locale('en')
+        })
+        pushOptions({
+          locale: 'en'
         })
 
         moment.months().forEach(function(month, index, months) {
           it('should be ' + month, function(done) {
-            settings.defaultDate = $.fullCalendar.moment(referenceDate).add(index, 'months')
-            settings.eventAfterAllRender = function() {
-              expect($('.fc-toolbar h2')).toContainText(month)
-              done()
-            }
-
-            $('#cal').fullCalendar(settings)
+            initCalendar({
+              defaultDate: $.fullCalendar.moment(referenceDate).add(index, 'months'),
+              eventAfterAllRender: function() {
+                expect($('.fc-toolbar h2')).toContainText(month)
+                done()
+              }
+            })
           })
         })
       })
@@ -42,26 +40,28 @@ describe('month name', function() {
       locales.forEach(function(locale, index, locales) {
         describe('when locale is ' + locale, function() {
           beforeEach(function() {
-            settings.locale = locale
             moment.locale(locale)
+          })
+          pushOptions({
+            locale: locale
           })
 
           moment.months().forEach(function(month, index, months) { // `month` will always be English
             it('should be the translated name for ' + month, function(done) {
               var localeMonths = moment.months()
               var localeMonth = localeMonths[index]
-
-              settings.defaultDate = $.fullCalendar.moment(referenceDate).add(index, 'months')
-              settings.eventAfterAllRender = function() {
-                if (viewClass === 'month') { // with month view check for occurence of the monthname in the title
-                  expect($('.fc-toolbar h2')).toContainText(localeMonth)
-                } else { // with day views ensure that title contains the properly formatted phrase
-                  expect($('.fc-toolbar h2')).toHaveText(settings.defaultDate.format('LL'))
+              var defaultDate = $.fullCalendar.moment(referenceDate).add(index, 'months')
+              initCalendar({
+                defaultDate: defaultDate,
+                eventAfterAllRender: function() {
+                  if (viewClass === 'month') { // with month view check for occurence of the monthname in the title
+                    expect($('.fc-toolbar h2')).toContainText(localeMonth)
+                  } else { // with day views ensure that title contains the properly formatted phrase
+                    expect($('.fc-toolbar h2')).toHaveText(defaultDate.format('LL'))
+                  }
+                  done()
                 }
-                done()
-              }
-
-              $('#cal').fullCalendar(settings)
+              })
             })
           })
         })
@@ -85,14 +85,14 @@ describe('month name', function() {
 
         months.forEach(function(month, index, months) { // `month` is our custom month name
           it('should be the translated name for ' + month, function(done) {
-            settings.defaultDate = $.fullCalendar.moment(referenceDate).add(index, 'months')
-            settings.monthNames = months
-            settings.eventAfterAllRender = function() {
-              expect($('.fc-toolbar h2')).toContainText(month)
-              done()
-            }
-
-            $('#cal').fullCalendar(settings)
+            initCalendar({
+              defaultDate: $.fullCalendar.moment(referenceDate).add(index, 'months'),
+              monthNames: months,
+              eventAfterAllRender: function() {
+                expect($('.fc-toolbar h2')).toContainText(month)
+                done()
+              }
+            })
           })
         })
       })
