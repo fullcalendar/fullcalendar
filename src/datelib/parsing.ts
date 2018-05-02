a96c5e57d99257b3c6123ff06c2d6b9742344056
Adam Shaw <arshaw@users.noreply.github.com>
2018-05-02 17:22:21 -0400

    fix parsing issues

--- src/datelib/parsing.ts
+++ src/datelib/parsing.ts
@@ -1,27 +1,31 @@
 
-const ISO_TIME_RE = /^\s*\d{4}-\d\d-\d\d[T ]\d/
+const ISO_START = /^\s*\d{4}-\d\d-\d\d([T ]\d)?/
 const ISO_TZO_RE = /(?:(Z)|([-+])(\d\d)(?::(\d\d))?)$/
-
-// TODO: accept a Date object somehow!!!
 
 export function parse(str) {
   let timeZoneOffset = null
   let hasTime = false
+  let m = ISO_START.exec(str)
 
-  if (ISO_TIME_RE.test(str)) {
-    hasTime = true
+  if (m) {
+    hasTime = Boolean(m[1])
 
-    str = str.replace(ISO_TZO_RE, function(whole, z, sign, minutes, seconds) {
-      if (z) {
-        timeZoneOffset = 0
-      } else {
-        timeZoneOffset = (
-          parseInt(minutes, 10) * 60 +
-          parseInt(seconds || 0, 10)
-        ) * (sign === '-' ? -1 : 1)
-      }
-      return ''
-    }) + '-00:00' // otherwise will parse in local
+    if (hasTime) {
+      str = str.replace(ISO_TZO_RE, function(whole, z, sign, minutes, seconds) {
+        if (z) {
+          timeZoneOffset = 0
+        } else {
+          timeZoneOffset = (
+            parseInt(minutes, 10) * 60 +
+            parseInt(seconds || 0, 10)
+          ) * (sign === '-' ? -1 : 1)
+        }
+        return ''
+      }) + 'Z' // otherwise will parse in local
+
+    } else {
+      str += 'T00:00:00Z'
+    }
   }
 
   return {
