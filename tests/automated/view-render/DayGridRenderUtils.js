79a561d6d423234518677a2cfde1e05b3212b431
kyle roux <jstacoder@gmail.com>
2018-07-09 01:43:13 -0700

    cleaned DayGridRenderUtils

--- tests/automated/view-render/DayGridRenderUtils.js
+++ tests/automated/view-render/DayGridRenderUtils.js
@@ -1,28 +1,34 @@
 import { formatIsoDay } from '../datelib/utils'
+import { 
+  DAY_GRID_CLASS, ROW_CLASS, 
+  BG_CLASS, HEAD_CLASS, 
+  VIEW_CLASS, DAY_HEADER_CLASS, 
+  DAY_TOP_CLASS, DAY_DISABLED_CLASS,
+  AXIS_CLASS, DAY_CLASS } from '../lib/constants'
 
 export function getDayEl(date) {
   if (typeof date === 'string') {
     date = new Date(date)
   }
-  return $('.fc-day-grid .fc-bg .fc-day[data-date="' + formatIsoDay(date) + '"]')
+  return $(`.${DAY_GRID_CLASS} .${BG_CLASS} .${DAY_CLASS}[data-date="${formatIsoDay(date)}"]`)
 }
 
 export function getDisabledEl(i) {
-  var el = $('.fc-day-grid .fc-bg .fc-disabled-day:eq(' + i + ')')
+  var el = $(`.${DAY_GRID_CLASS} .${BG_CLASS} .${DAY_DISABLED_CLASS}:eq(${i})`)
   expect(el).toHaveLength(1)
   return el
 }
 
 export function getDayEls() {
-  return $('.fc-day-header')
+  return $(`.${DAY_HEADER_CLASS}`)
 }
 
 export function getDayElTopElText(date) {
-  return $(`td.fc-day-top[data-date="${date}"]`).text()
+  return $(`td.${DAY_TOP_CLASS}[data-date="${date}"]`).text()
 }
 
 export function getHeaderEl() {
-  return $('.fc-view > table > .fc-head')
+  return $(`.${VIEW_CLASS} > table > .${HEAD_CLASS}`)
 }
 
 export function hasHeaderEl() {
@@ -35,4 +41,8 @@ export function getFirstDayEl() {
 
 export function getLastDayEl() {
   return getDayEls().last()
+}
+
+export function getAllDayAxisEl(){
+  return $(`.${DAY_GRID_CLASS} > .${ROW_CLASS} > .${BG_CLASS} .${AXIS_CLASS}`)
 }
