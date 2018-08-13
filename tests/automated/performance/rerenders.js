7ecdd96667eb0b30b1879fba077c84d93a7f5061
Adam Shaw <arshaw@users.noreply.github.com>
2018-08-12 23:09:09 -0400

    nice performance

--- tests/automated/performance/rerenders.js
+++ tests/automated/performance/rerenders.js
@@ -1,12 +1,4 @@
 
-// HACK: we know Scheduler introduces an extra rerender :(
-const SKIP_RERENDERS = Boolean(FullCalendar.schedulerVersion)
-if (SKIP_RERENDERS) {
-  console.log('skipping rerenders')
-}
-
-// eslint-disable-next-line
-SKIP_RERENDERS ||
 describe('rerender performance', function() {
 
   pushOptions({
@@ -39,46 +31,46 @@ describe('rerender performance', function() {
         var Class = FullCalendar[className]
 
         it('calls methods a limited number of times', function(done) {
-          var executeDateRender = spyOnMethod(Class, 'executeDateRender')
-          var renderEventRanges = spyOnMethod(Class, 'renderEventRanges')
+          var renderDates = spyOnMethod(Class, 'renderDates')
+          var renderEvents = spyOnMethod(Class, 'renderEvents')
           var updateSize = spyOnMethod(Class, 'updateSize')
 
           initCalendar({
             defaultView: settings.defaultView
           })
 
-          expect(executeDateRender.calls.count()).toBe(1)
-          expect(renderEventRanges.calls.count()).toBe(1)
+          expect(renderDates.calls.count()).toBe(1)
+          expect(renderEvents.calls.count()).toBe(1)
           expect(updateSize.calls.count()).toBe(1)
 
           currentCalendar.changeView(settings.changeToView)
 
-          expect(executeDateRender.calls.count()).toBe(1)
-          expect(renderEventRanges.calls.count()).toBe(1)
-          expect(updateSize.calls.count()).toBe(2) // +1
+          expect(renderDates.calls.count()).toBe(1)
+          expect(renderEvents.calls.count()).toBe(1)
+          expect(updateSize.calls.count()).toBe(1)
 
           currentCalendar.changeView(settings.defaultView)
 
-          expect(executeDateRender.calls.count()).toBe(2) // +1
-          expect(renderEventRanges.calls.count()).toBe(2) // +1
-          expect(updateSize.calls.count()).toBe(3) // +1
+          expect(renderDates.calls.count()).toBe(2) // +1
+          expect(renderEvents.calls.count()).toBe(2) // +1
+          expect(updateSize.calls.count()).toBe(2) // +1
 
           currentCalendar.rerenderEvents()
 
-          expect(executeDateRender.calls.count()).toBe(2)
-          expect(renderEventRanges.calls.count()).toBe(3) // +1
-          expect(updateSize.calls.count()).toBe(5) // +2, TODO: get to just +1
+          expect(renderDates.calls.count()).toBe(2)
+          expect(renderEvents.calls.count()).toBe(3) // +1
+          expect(updateSize.calls.count()).toBe(3) // +1
 
           $(window).simulate('resize')
 
           setTimeout(function() {
 
-            expect(executeDateRender.calls.count()).toBe(2)
-            expect(renderEventRanges.calls.count()).toBe(3)
-            expect(updateSize.calls.count()).toBe(6) // +1
+            expect(renderDates.calls.count()).toBe(2)
+            expect(renderEvents.calls.count()).toBe(3)
+            expect(updateSize.calls.count()).toBe(4) // +1
 
-            executeDateRender.restore()
-            renderEventRanges.restore()
+            renderDates.restore()
+            renderEvents.restore()
             updateSize.restore()
 
             done()
