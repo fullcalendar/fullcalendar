531181680b12d69546245d12b5d36cade994decc
Adam Shaw <arshaw@users.noreply.github.com>
2018-08-16 01:06:59 -0400

    when event el destroyed, trigger eventMouseout

--- src/interactions/EventHovering.ts
+++ src/interactions/EventHovering.ts
@@ -1,4 +1,4 @@
-import DateComponent from '../component/DateComponent'
+import DateComponent, { Seg } from '../component/DateComponent'
 import { listenToHoverBySelector } from '../util/dom-event'
 import { getElSeg } from '../component/renderers/EventRenderer'
 import EventApi from '../api/EventApi'
@@ -10,36 +10,54 @@ enters/leaves event-elements of a component.
 export default class EventHovering {
 
   component: DateComponent
-  destroy: () => void
+  removeHoverListeners: () => void
+  currentSegEl: HTMLElement
 
   constructor(component: DateComponent) {
     this.component = component
 
-    this.destroy = listenToHoverBySelector(
+    this.removeHoverListeners = listenToHoverBySelector(
       component.el,
       component.segSelector,
       this.handleSegEnter,
       this.handleSegLeave
     )
+
+    component.getCalendar().on('eventDestroy', this.handleEventDestroy)
+  }
+
+  destroy() {
+    this.removeHoverListeners()
+    this.component.getCalendar().off('eventDestroy', this.handleEventDestroy)
+  }
+
+  // for simulating an eventMouseout when the event el is destroyed while mouse is over it
+  handleEventDestroy = (arg) => {
+    if (arg.el === this.currentSegEl) {
+      this.handleSegLeave(null, this.currentSegEl)
+    }
   }
 
   handleSegEnter = (ev: Event, segEl: HTMLElement) => {
     segEl.classList.add('fc-allow-mouse-resize')
+    this.currentSegEl = segEl
     this.triggerEvent('eventMouseover', ev, segEl)
   }
 
-  handleSegLeave = (ev: Event, segEl: HTMLElement) => {
+  handleSegLeave = (ev: Event | null, segEl: HTMLElement) => {
     segEl.classList.remove('fc-allow-mouse-resize')
+    this.currentSegEl = null
     this.triggerEvent('eventMouseout', ev, segEl)
   }
 
-  triggerEvent(publicEvName: string, ev: Event, segEl: HTMLElement) {
+  triggerEvent(publicEvName: string, ev: Event | null, segEl: HTMLElement) {
     let { component } = this
     let seg = getElSeg(segEl)!
 
-    if (component.isValidSegDownEl(ev.target as HTMLElement)) {
+    if (!ev || component.isValidSegDownEl(ev.target as HTMLElement)) {
       component.publiclyTrigger(publicEvName, [
         {
+          el: segEl,
           event: new EventApi(
             this.component.getCalendar(),
             seg.eventRange.eventDef,
