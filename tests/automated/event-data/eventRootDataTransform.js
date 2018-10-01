261ec00934b5a802e394e0f856de2c7d8bbf5310
Adam Shaw <arshaw@users.noreply.github.com>
2018-10-01 19:42:11 -0400

    test for eventRootDataTransform

---
+++ tests/automated/event-data/eventRootDataTransform.js
@@ -0,0 +1,41 @@
+describe('eventRootDataTransform', function() {
+
+  const FETCH_FUNC = function(info, successCallback) {
+    successCallback({
+      something: [
+        { title: 'hi', start: '2018-10-01' }
+      ]
+    })
+  }
+
+  const TRANSFORM = function(input) {
+    return input.something
+  }
+
+  pushOptions({
+    defaultDate: '2018-10-01'
+  })
+
+  it('massages event data with calendar-wide setting', function() {
+    initCalendar({
+      eventSources: [ FETCH_FUNC ],
+      eventRootDataTransform: TRANSFORM
+    })
+
+    expect(currentCalendar.getEvents().length).toBe(1)
+  })
+
+  it('massages event data with source setting', function() {
+    initCalendar({
+      eventSources: [
+        {
+          events: FETCH_FUNC,
+          rootDataTransform: TRANSFORM
+        }
+      ]
+    })
+
+    expect(currentCalendar.getEvents().length).toBe(1)
+  })
+
+})
