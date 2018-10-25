848078bdd1ec64bc84964ebbb27c65d6120f0e2a
Adam Shaw <arshaw@users.noreply.github.com>
2018-10-25 14:35:26 -0400

    fix tests

--- tests/automated/view-dates/ViewDateUtils.js
+++ tests/automated/view-dates/ViewDateUtils.js
@@ -1,7 +1,7 @@
 
 export function expectRenderRange(start, end) {
   var currentView = currentCalendar.getView()
-  var dateProfile = currentView.dateProfile
+  var dateProfile = currentView.props.dateProfile
 
   expect(dateProfile.renderRange.start).toEqualDate(start)
   expect(dateProfile.renderRange.end).toEqualDate(end)
@@ -10,7 +10,7 @@ export function expectRenderRange(start, end) {
 
 export function expectActiveRange(start, end) {
   var currentView = currentCalendar.getView()
-  var dateProfile = currentView.dateProfile
+  var dateProfile = currentView.props.dateProfile
 
   expect(dateProfile.activeRange.start).toEqualDate(start)
   expect(dateProfile.activeRange.end).toEqualDate(end)
