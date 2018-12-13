26e2a2766d5ffebc7be3c298c2abb5686397d75f
Adam Shaw <arshaw@users.noreply.github.com>
2018-12-13 17:59:13 -0500

    eventDragMutationMassager gets calendar

--- src/interactions/EventDragging.ts
+++ src/interactions/EventDragging.ts
@@ -139,7 +139,12 @@ export default class EventDragging { // TODO: rename to EventSelectingAndDraggin
         initialCalendar === receivingCalendar ||
         receivingComponent.opt('editable') && receivingComponent.opt('droppable')
       ) {
-        mutation = computeEventMutation(initialHit, hit, receivingCalendar.pluginSystem.hooks.eventDragMutationMassagers)
+        mutation = computeEventMutation(
+          initialHit,
+          hit,
+          receivingCalendar.pluginSystem.hooks.eventDragMutationMassagers,
+          receivingCalendar
+        )
 
         if (mutation) {
           mutatedRelevantEvents = applyMutationToEventStore(relevantEvents, mutation, receivingCalendar)
@@ -356,9 +361,9 @@ export default class EventDragging { // TODO: rename to EventSelectingAndDraggin
 }
 
 
-export type eventDragMutationMassager = (mutation: EventMutation, hit0: Hit, hit1: Hit) => void
+export type eventDragMutationMassager = (mutation: EventMutation, hit0: Hit, hit1: Hit, calendar: Calendar) => void
 
-function computeEventMutation(hit0: Hit, hit1: Hit, massagers: eventDragMutationMassager[]): EventMutation {
+function computeEventMutation(hit0: Hit, hit1: Hit, massagers: eventDragMutationMassager[], calendar: Calendar): EventMutation {
   let dateSpan0 = hit0.dateSpan
   let dateSpan1 = hit1.dateSpan
   let date0 = dateSpan0.range.start
@@ -393,7 +398,7 @@ function computeEventMutation(hit0: Hit, hit1: Hit, massagers: eventDragMutation
   }
 
   for (let massager of massagers) {
-    massager(mutation, hit0, hit1)
+    massager(mutation, hit0, hit1, calendar)
   }
 
   return mutation
