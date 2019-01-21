e9d19772f0e8cda0535f61075e7ecabf72d4eb86
Adam Shaw <arshaw@users.noreply.github.com>
2019-01-21 18:32:57 -0300

    improve sass-args script

--- bin/sass-args.js
+++ bin/sass-args.js
@@ -8,10 +8,13 @@ let parts = []
 
 for (let packageName in packagePaths) {
   let jsPath = packagePaths[packageName][0]
-  let srcPath = path.dirname(jsPath)
-  let distPath = 'dist/' + packageName
 
-  parts.push(srcPath + ':' + distPath)
+  if (jsPath.match(/^src\//)) { // one of our lib files
+    let srcPath = path.dirname(jsPath)
+    let distPath = 'dist/' + packageName
+
+    parts.push(srcPath + ':' + distPath)
+  }
 }
 
 console.log(parts.join(' '))
