46558261962126be78e25e6feda73ddcaa3cf3ff
Adam Shaw <arshaw@users.noreply.github.com>
2019-01-31 19:54:29 -0300

    fix code in multi-array util

--- rollup-plugin-multi-entry-array.js
+++ rollup-plugin-multi-entry-array.js
@@ -1,49 +1,49 @@
-// TODO: add license info at top of this file
-// code ripped from https://github.com/rollup/rollup-plugin-multi-entry/blob/master/src/index.js
+// ripped from https://github.com/rollup/rollup-plugin-multi-entry/blob/master/src/index.js
+// released under the MIT license: https://opensource.org/licenses/MIT
 
-import { promise as matched } from 'matched'; // TODO: add to package.json
+import { promise as matched } from 'matched' // TODO: add to package.json
 
-const entry = '\0array-entry';
+const entry = '\0array-entry'
 
 export default function multiEntry(config) {
-  let include = [];
-  let exclude = [];
+  let include = []
+  let exclude = []
 
   function configure(config) {
     if (typeof config === 'string') {
-      include = [config];
+      include = [config]
     } else if (Array.isArray(config)) {
-      include = config;
+      include = config
     } else {
-      include = config.include || [];
-      exclude = config.exclude || [];
+      include = config.include || []
+      exclude = config.exclude || []
     }
   }
 
   if (config) {
-    configure(config);
+    configure(config)
   }
 
   return {
     options(options) {
       if (options.input && options.input !== entry) {
-        configure(options.input);
+        configure(options.input)
       }
-      options.input = entry;
+      options.input = entry
     },
 
     resolveId(id) {
       if (id === entry) {
-        return entry;
+        return entry
       }
     },
 
     load(id) {
       if (id === entry) {
         if (!include.length) {
-          return Promise.resolve('');
+          return Promise.resolve('')
         }
-        const patterns = include.concat(exclude.map(pattern => '!' + pattern));
+        const patterns = include.concat(exclude.map(pattern => '!' + pattern))
         return matched(patterns, { realpath: true }).then(paths => {
           let s =
             paths.map((path, index) => `import _m${index} from ${JSON.stringify(path)}`).join(';\n') +
@@ -52,8 +52,8 @@ export default function multiEntry(config) {
             paths.map((path, index) => `_m${index}`).join(', ') + '\n' +
             '];\n'
           return s
-        });
+        })
       }
     }
-  };
+  }
 }
