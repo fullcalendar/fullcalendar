160a26dd9f8a16b408b740b447f606f2d90360c5
Adam Shaw <arshaw@users.noreply.github.com>
2019-02-05 18:33:55 -0300

    fix plugin problem with tests

--- tests/automated/datelib/main.js
+++ tests/automated/datelib/main.js
@@ -1,13 +1,14 @@
 import { formatPrettyTimeZoneOffset, formatIsoTimeZoneOffset, formatIsoWithoutTz } from './utils'
 import { getDSTDeadZone } from './dst-dead-zone'
 import { DateEnv, createFormatter, createDuration, startOfDay, diffWholeWeeks, diffWholeDays, diffDayAndTime, Calendar } from '@fullcalendar/core'
+import dayGridPlugin from '@fullcalendar/daygrid'
 
 describe('datelib', function() {
   let enLocale
 
   beforeEach(function() {
     enLocale = new Calendar(document.createElement('div'), { // HACK
-      plugins: [ 'daygrid' ]
+      plugins: [ dayGridPlugin ]
     }).dateEnv.locale
   })
 
