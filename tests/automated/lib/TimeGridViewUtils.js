56040f5906e75098d9cd90e3904799fbe3326ab7
Adam Shaw <arshaw@users.noreply.github.com>
2019-03-27 18:38:26 -0500

    tests for resetOptions and setOptions

--- tests/automated/lib/TimeGridViewUtils.js
+++ tests/automated/lib/TimeGridViewUtils.js
@@ -1,5 +1,14 @@
 
-// this file is about DAY grid???
+export function getTimeGridScroller() { // returns DOM node
+  return document.querySelector('.fc-scroller.fc-time-grid-container')
+}
+
+export function allDaySlotDisplayed() {
+  return Boolean($('.fc-timeGrid-view .fc-day-grid').length)
+}
+
+
+// about DAYGRID???...
 
 const DAY_GRID_CLASS = 'fc-day-grid'
 
