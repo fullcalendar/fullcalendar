2c3fc89f3a4fa1a0a129a9203d0da5ea48b9ad2d
Adam Shaw <arshaw@users.noreply.github.com>
2019-04-03 00:03:34 -0500

    version 4.0.2

---
+++ dist/moment-timezone/main.d.ts
@@ -0,0 +1,5 @@
+declare module "@fullcalendar/moment-timezone" {
+    import "@fullcalendar/moment-timezone/builds/moment-timezone-with-data";
+    const _default_12: import("@fullcalendar/core/plugin-system").PluginDef;
+    export default _default_12;
+}
