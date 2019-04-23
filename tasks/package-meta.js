1d0228e63237b59d8118921cc54bd7cf0b6beefc
Adam Shaw <arshaw@users.noreply.github.com>
2019-04-23 17:10:50 -0500

    reference other fc packages with tilde. fixes #4620

--- tasks/package-meta.js
+++ tasks/package-meta.js
@@ -15,7 +15,7 @@ if (rootPackageVersion.indexOf('-') !== -1) {
   console.log('Prerelease detected. Using exact version precision.')
   versionPrecision = ''
 } else {
-  versionPrecision = '^'
+  versionPrecision = '~'
 }
 
 
