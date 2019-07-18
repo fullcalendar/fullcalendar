5dcf4120942c9f347872e238874419227c54332b
Lucas Bornegrim <lucas.bornegrim@gmail.com>
2019-07-18 12:20:07 +0200

    will fix srr issues nuxt.js

--- src/core/util/dom-manip.ts
+++ src/core/util/dom-manip.ts
@@ -117,25 +117,27 @@ export function removeElement(el: HTMLElement) {
 // ----------------------------------------------------------------------------------------------------------------
 
 // from https://developer.mozilla.org/en-US/docs/Web/API/Element/closest
-const matchesMethod =
-  Element.prototype.matches ||
-  (Element.prototype as any).matchesSelector ||
-  (Element.prototype as any).msMatchesSelector
+const matchesMethod=function(s) {
+  var matches = (this.document || this.ownerDocument).querySelectorAll(s),
+  i = matches.length;
+  while (--i >= 0 && matches.item(i) !== this) {}
+  return i > -1;
+  };
 
-const closestMethod = Element.prototype.closest || function(selector) {
+const closestMethod = function (selector) {
   // polyfill
-  let el = this
+  var el = this;
   if (!document.documentElement.contains(el)) {
-    return null
+  return null;
   }
   do {
-    if (elementMatches(el, selector)) {
-      return el
-    }
-    el = el.parentElement || el.parentNode
-  } while (el !== null && el.nodeType === 1)
-  return null
-}
+  if (elementMatches(el, selector)) {
+  return el;
+  }
+  el = el.parentElement || el.parentNode;
+  } while (el !== null && el.nodeType === 1);
+  return null;
+  };
 
 export function elementClosest(el: HTMLElement, selector: string): HTMLElement {
   return closestMethod.call(el, selector)
