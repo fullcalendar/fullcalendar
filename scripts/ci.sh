e87a81640e81313c86f372f94bb3cb182c21fb8e
Adam Shaw <arshaw@users.noreply.github.com>
2019-07-24 17:51:07 -0400

    exclude angular from ci

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -7,10 +7,20 @@ npm run build
 npm run test:single
 npm run lint
 
-# TODO: make --no-parallel the default for monorepo-tool
+EXCLUDE_PKGS=''
+
+# all angular-related packages have e2e tests that require a complicated CI setup.
+# (see .travis.yml in each project). Skip altogether for now.
+if [[ "$CI" == "true" ]]
+then
+  echo "Skipping angular because we're in a CI environment"
+  EXCLUDE_PKGS='**/angular'
+fi
 
 # all contrib projects build/test/lint/etc?
-npx monorepo run ci --filter-pkgs 'packages-contrib/*' --no-parallel
+npx monorepo run ci --filter-pkgs 'packages-contrib/*' --exclude-pkgs "$EXCLUDE_PKGS" --no-parallel
 
 # all example projects can build?
-npx monorepo run build --filter-pkgs 'example-projects/*' --no-parallel
+npx monorepo run build --filter-pkgs 'example-projects/*' --exclude-pkgs "$EXCLUDE_PKGS" --no-parallel
+
+# TODO: make --no-parallel the default for monorepo-tool
