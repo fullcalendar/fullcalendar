a788532ec0ffe1562e3fe8f512a4fc7a31661265
Anuruk Surin <idrafttt@gmail.com>
2019-10-08 11:16:39 +0700

    update locale th.js

--- packages/core/src/locales/th.js
+++ packages/core/src/locales/th.js
@@ -2,7 +2,6 @@
 export default {
   code: "th",
   week: {
-    // GB/T 7408-1994《数据元和交换格式·信息交换·日期和时间表示法》与ISO 8601:1988等效
     dow: 1, // Monday is the first day of the week.
     doy: 4  // The week that contains Jan 4th is the first week of the year.
   },
