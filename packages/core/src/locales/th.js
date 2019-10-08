53e6fb6780198ca148e533973ddf02fc3d7a0b01
Anuruk Surin <idrafttt@gmail.com>
2019-10-08 10:59:41 +0700

    update locale th.js

--- packages/core/src/locales/th.js
+++ packages/core/src/locales/th.js
@@ -1,6 +1,11 @@
 
 export default {
   code: "th",
+  week: {
+    // GB/T 7408-1994《数据元和交换格式·信息交换·日期和时间表示法》与ISO 8601:1988等效
+    dow: 1, // Monday is the first day of the week.
+    doy: 4  // The week that contains Jan 4th is the first week of the year.
+  },
   buttonText: {
     prev: "ย้อน",
     next: "ถัดไป",
@@ -8,8 +13,9 @@ export default {
     month: "เดือน",
     week: "สัปดาห์",
     day: "วัน",
-    list: "แผนงาน"
+    list: "กำหนดการ"
   },
+  weekLabel: "สัปดาห์",
   allDayText: "ตลอดวัน",
   eventLimitText: "เพิ่มเติม",
   noEventsMessage: "ไม่มีกิจกรรมที่จะแสดง"
