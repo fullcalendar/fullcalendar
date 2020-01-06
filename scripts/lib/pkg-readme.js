6c0ac029c289fed109bc563600106cc3913798d5
Adam Shaw <arshaw@users.noreply.github.com>
2020-01-06 11:55:43 -0500

    use existing README.md if exists

--- scripts/lib/pkg-readme.js
+++ scripts/lib/pkg-readme.js
@@ -1,23 +1,35 @@
 const path = require('path')
 const handleBars = require('handlebars')
-const { readFile, writeFile } = require('./util')
+const { readFile, writeFile, fileExists } = require('./util')
 const { pkgStructs } = require('./pkg-struct')
 
 
 exports.writePkgReadmes = writePkgReadmes
 
 
-function writePkgReadmes() {
-  return readFile('packages/README.md.tpl').then((templateText) => {
-    return handleBars.compile(templateText)
-  }).then((template) => {
-    return Promise.all(
-      pkgStructs.map((pkgStruct) => {
-        let readmeText = template(pkgStruct.jsonObj)
-        let readmePath = path.join(pkgStruct.distDir, 'README.md')
+async function writePkgReadmes() {
+  let templateText = await readFile('packages/README.md.tpl')
+  let template = handleBars.compile(templateText)
 
-        return writeFile(readmePath, readmeText)
-      })
-    )
-  })
+  return Promise.all(
+    pkgStructs.map((pkgStruct) => {
+      return writePkgReadme(pkgStruct, template)
+    })
+  )
+}
+
+
+async function writePkgReadme(pkgStruct, template) {
+  let readmeDestPath = path.join(pkgStruct.distDir, 'README.md')
+  let ownReadmePath = path.join(pkgStruct.dir, 'README.md')
+  let hasOwnReadme = await fileExists(ownReadmePath)
+  let readmeText
+
+  if (hasOwnReadme) {
+    readmeText = await readFile(ownReadmePath)
+  } else {
+    readmeText = template(pkgStruct.jsonObj)
+  }
+
+  return writeFile(readmeDestPath, readmeText)
 }
