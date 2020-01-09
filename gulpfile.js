dfa9946d79de973547b3a6ac7c02ab5299d40811
Adam Shaw <arshaw@users.noreply.github.com>
2020-01-09 14:02:23 -0500

    dont double-compile tsc for watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 const { parallel, series } = require('gulp')
-const { shellTask, watch } = require('./scripts/lib/util')
+const { shellTask, whenFirstModified, fileExists, mkdirp } = require('./scripts/lib/util')
 const { writePkgJsons } = require('./scripts/lib/pkg-json-write')
 const { bundlPkgDefs } = require('./scripts/lib/pkg-dts')
 const { writePkgReadmes } = require('./scripts/lib/pkg-readme')
 const { writePkgLicenses } = require('./scripts/lib/pkg-license')
-const { minifyJs, minifyCss } = require('./scripts/lib/minify') // combine into one task?
+const { minifyJs, minifyCss } = require('./scripts/lib/minify') // combine into one task? make part of rollup?
 const { lint } = require('./scripts/lib/lint')
 const { archive } = require('./scripts/lib/archive')
 const { copyScss, watchScss } = require('./scripts/lib/pkg-scss') // watchScss is a bad name!
@@ -25,32 +25,47 @@ exports.minify = parallel(minifyJs, minifyCss)
 
 exports.build = series(
   writePkgJsons, // important for node-resolution
+  copyScss,
   shellTask('npm:tsc'),
+  writeLocales, // needs tsc
   parallel(
-    shellTask('npm:rollup'),
+    shellTask('npm:rollup'), // needs tsc, copied scss, generated locales
     writePkgLicenses,
     writePkgReadmes,
-    writeLocales, // needs tsc
-    copyScss,
     buildDts
   ),
   parallel(minifyJs, minifyCss)
 )
 
-
 exports.watch = series(
-  writePkgJsons, // important for node-resolution. doesn't watch!
-  shellTask('npm:tsc:debug'),
+  writePkgJsons, // important for node-resolution
+  copyScss,
   parallel(
-    shellTask('npm:tsc:watch'), // TODO: better system. does two consecutive compiles
-    shellTask('npm:rollup:watch'),
-    writePkgLicenses, // doesn't watch!
-    writePkgReadmes, // doesn't watch!
-    watchLocales,
-    watchScss,
-    watchLocales, // needs tsc
-    watchScss,
-    buildDts // doesn't watch!
+    shellTask('npm:tsc:watch'),
+    series(
+      waitTsc,
+      writeLocales, // needs tsc
+      parallel(
+        shellTask('npm:rollup:watch'), // needs tsc, copied scss, generated locales
+        writePkgLicenses, // doesn't watch!
+        writePkgReadmes, // doesn't watch!
+        buildDts, // doesn't watch!
+        watchLocales, // TODO: ignore initial
+        watchScss // TODO: ignore initial
+      )
+    )
   )
-  // doesn't minify!
-)
+) // doesn't minify!
+// BUG: right after clean, when watching, tsc re-compiles a lot (must be watching something)
+
+
+async function waitTsc() {
+  let dir = 'tmp/tsc-output' // TODO: make a constant
+  let dirExists = await fileExists(dir)
+
+  if (!dirExists) {
+    await mkdirp(dir)
+  }
+
+  return whenFirstModified(dir, { delay: 1000, disableGlobbing: true })
+}
--- scripts/lib/util.js
+++ scripts/lib/util.js
@@ -9,7 +9,9 @@ const mkdirp = util.promisify(require('mkdirp'))
 const concurrently = require('concurrently')
 const { watch } = require('gulp')
 
+exports.whenFirstModified = whenFirstModified
 exports.watch = betterWatch
+exports.watchTask = watchTask
 exports.shellTask = shellTask
 exports.promisifyVinyl = promisifyVinyl
 exports.readFile = betterReadFile
@@ -17,6 +19,7 @@ exports.writeFile = betterWriteFile
 exports.writeFileSync = betterWriteFileSync
 exports.copyFile = betterCopyFile
 exports.fileExists = fileExists
+exports.mkdirp = mkdirp
 
 
 function betterWatch() {
@@ -28,6 +31,34 @@ function betterWatch() {
       resolve()
     })
   })
+}
+
+
+// function firstModifiedTask() { // TODO: general taskify (with description?)
+//   let args = arguments
+//   return function() {
+//     return whenFirstModified.apply(null, args) // can just use bind?
+//   }
+// }
+
+
+function whenFirstModified(glob, options) {
+  return new Promise((resolve) => {
+    let watcher = watch(glob, options || {}, function(cb) {
+      watcher.close()
+      resolve()
+      cb()
+    })
+  })
+}
+
+
+function watchTask() { // TODO: use in more places
+  let watchArgs = arguments
+
+  return function() {
+    return betterWatch.apply(null, watchArgs)
+  }
 }
 
 
