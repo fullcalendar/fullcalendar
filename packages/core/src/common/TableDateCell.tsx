76cd83d11a8a473a85ddbbcab312ab762c6e50ab
Adam Shaw <arshaw@users.noreply.github.com>
2020-03-15 19:52:19 -0400

    dayLabel* hooks

--- packages/core/src/common/TableDateCell.tsx
+++ packages/core/src/common/TableDateCell.tsx
@@ -1,5 +1,5 @@
-import { rangeContainsMarker, DateRange } from '../datelib/date-range'
-import { getDayClassNames, getDayMeta } from '../component/date-rendering'
+import { DateRange } from '../datelib/date-range'
+import { getDayClassNames, getDayMeta, DateMeta } from '../component/date-rendering'
 import { DateMarker } from '../datelib/marker'
 import { DateProfile } from '../DateProfileGenerator'
 import ComponentContext from '../component/ComponentContext'
@@ -9,6 +9,7 @@ import { DateFormatter, formatDayString } from '../datelib/formatting'
 import { BaseComponent } from '../vdom-util'
 import { RenderHook } from './render-hook'
 import { buildNavLinkData } from './nav-link'
+import ViewApi from '../ViewApi'
 
 
 export interface TableDateCellProps {
@@ -23,72 +24,72 @@ export interface TableDateCellProps {
   extraDataAttrs?: object
 }
 
+interface MountProps {
+  date: Date
+  view: ViewApi
+  [otherProp: string]: any
+}
+
+type DynamicProps = MountProps & DateMeta & {
+  text: string
+  navLinkData: string
+}
+
+
 export default class TableDateCell extends BaseComponent<TableDateCellProps> { // BAD name for this class now. used in the Header
 
   render(props: TableDateCellProps, state: {}, context: ComponentContext) {
     let { dateEnv, options } = context
-    let { date, dateProfile, isDateDistinct } = props
-    let isDateValid = rangeContainsMarker(dateProfile.activeRange, date) // TODO: called too frequently. cache somehow.
-    let innerText
-
-    if (typeof options.columnHeaderText === 'function') {
-      innerText = options.columnHeaderText(
-        dateEnv.toDate(date)
-      )
-    } else {
-      innerText = dateEnv.format(date, props.colHeadFormat)
-    }
-
+    let { date, isDateDistinct } = props
     let dayMeta = isDateDistinct // if only one row of days, the classNames on the header can represent the specific days beneath
       ? getDayMeta(date, props.todayRange, props.dateProfile)
       : getDayMeta(date)
 
     let classNames = [ 'fc-day-header' ].concat(getDayClassNames(dayMeta, context.theme))
-    let attrs = {} as any
+    let text = dateEnv.format(date, props.colHeadFormat)
 
-    if (isDateValid && isDateDistinct) {
-      attrs['data-date'] = formatDayString(date)
-    }
+    // if colCnt is 1, we are already in a day-view and don't need a navlink
+    let navLinkData = (options.navLinks && !dayMeta.isDisabled && isDateDistinct && props.colCnt > 1)
+      ? buildNavLinkData(date)
+      : null
 
-    if (props.colSpan > 1) {
-      attrs.colSpan = props.colSpan
-    }
-
-    if (props.extraDataAttrs) {
-      __assign(attrs, props.extraDataAttrs)
-    }
-
-    let mountProps = {
+    let mountProps: MountProps = {
       date: dateEnv.toDate(date),
       view: context.view,
       ...props.extraMountProps
     }
-    let dynamicProps = {
+
+    let dynamicProps: DynamicProps = {
       ...mountProps,
+      text,
+      navLinkData,
       ...dayMeta
     }
 
-    // if colCnt is 1, we are already in a day-view and don't need a navlink
-    let navLinkData = (options.navLinks && isDateValid && isDateDistinct && props.colCnt > 1)
-      ? buildNavLinkData(date)
-      : null
-
     return (
-      <RenderHook name='dateHeader' mountProps={mountProps} dynamicProps={dynamicProps}>
+      <RenderHook name='dayLabel' mountProps={mountProps} dynamicProps={dynamicProps} defaultInnerContent={renderInner}>
         {(rootElRef, customClassNames, innerElRef, innerContent) => (
-          <th class={classNames.concat(customClassNames).join(' ')} {...attrs} ref={rootElRef}>
-            {isDateValid &&
-              <a data-navlink={navLinkData}>
-                {innerText}
-              </a>
-            }
-            {innerContent &&
-              <div class='date-header-misc' ref={innerElRef}>{innerContent}</div>
-            }
-          </th>
+          <th
+            ref={rootElRef}
+            className={classNames.concat(customClassNames).join(' ')}
+            data-date={(!dayMeta.isDisabled && isDateDistinct) ? formatDayString(date) : undefined}
+            colSpan={props.colSpan > 1 ? props.colSpan : undefined}
+            {...props.extraDataAttrs}
+          >{innerContent}</th>
         )}
       </RenderHook>
     )
   }
 
+}
+
+
+function renderInner(dynamicProps: DynamicProps) {
+  if (!dynamicProps.isDisabled) {
+    return (
+      <a data-navlink={dynamicProps.navLinkData}>
+        {dynamicProps.text}
+      </a>
+    )
+  }
 }
