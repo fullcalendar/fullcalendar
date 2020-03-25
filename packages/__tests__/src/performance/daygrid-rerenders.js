fabfb995c1c89a9b273a6b65a73fe138e8eec0f1
Adam Shaw <arshaw@users.noreply.github.com>
2020-03-25 00:01:27 -0400

    change how daygrid-rerenders is done

--- packages/__tests__/src/performance/daygrid-rerenders.js
+++ packages/__tests__/src/performance/daygrid-rerenders.js
@@ -1,18 +1,24 @@
-import { DayHeader } from '@fullcalendar/core'
-import { Table } from '@fullcalendar/daygrid'
-import ComponentSpy from '../lib/ComponentSpy'
-
 
 it('daygrid view rerenders well', function(done) {
-  let headerSpy = new ComponentSpy(DayHeader)
-  let gridSpy = new ComponentSpy(Table)
+  let dayLabelMountCnt = 0
+  let dayLabelRenderCnt = 0
+  let dayCellRenderCnt = 0
   let eventRenderCnt = 0
 
   initCalendar({
     defaultView: 'dayGridMonth',
     defaultDate: '2017-10-04',
     windowResizeDelay: 0,
-    eventRender() {
+    dayLabelDidMount() {
+      dayLabelMountCnt++
+    },
+    dayLabelContent() {
+      dayLabelRenderCnt++
+    },
+    dayCellContent() {
+      dayCellRenderCnt++
+    },
+    eventContent() {
       eventRenderCnt++
     },
     events: [
@@ -21,52 +27,41 @@ it('daygrid view rerenders well', function(done) {
   })
 
   function resetCounts() {
-    headerSpy.resetCounts()
-    gridSpy.resetCounts()
+    dayLabelMountCnt = 0
+    dayLabelRenderCnt = 0
+    dayCellRenderCnt = 0
     eventRenderCnt = 0
   }
 
-  function expectHeaderRendered(bool) {
-    expect(headerSpy.renderCount).toBe(bool ? 1 : 0)
-  }
-
-  function expectGridRendered(bool) {
-    // 2nd render is for receiving clientWidth, 2nd is for shrinkWidth. TODO: optimize!
-    expect(gridSpy.renderCount).toBeLessThanOrEqual(bool ? 3 : 0)
-  }
-
-  expectHeaderRendered(true)
-  expectGridRendered(true)
+  expect(dayLabelMountCnt).toBe(7)
+  expect(dayLabelRenderCnt).toBe(7)
+  expect(dayCellRenderCnt).toBe(42)
   expect(eventRenderCnt).toBe(1)
 
   resetCounts()
   currentCalendar.next()
-  expectHeaderRendered(true)
-  expectGridRendered(true)
+
+  expect(dayLabelMountCnt).toBe(0) // same header across months...
+  expect(dayLabelRenderCnt).toBe(7) // ...but still rerendered inner content
+  expect(dayCellRenderCnt).toBe(42)
   expect(eventRenderCnt).toBe(0) // event will be out of view
 
-  resetCounts()
   currentCalendar.changeView('listWeek') // switch away
-  expectHeaderRendered(false)
-  expectGridRendered(false)
-  expect(eventRenderCnt).toBe(0)
-
   resetCounts()
   currentCalendar.changeView('dayGridMonth') // return to view
-  expectHeaderRendered(true)
-  expectGridRendered(true)
+  expect(dayLabelMountCnt).toBe(7)
+  expect(dayLabelRenderCnt).toBe(7)
+  expect(dayCellRenderCnt).toBe(42)
   expect(eventRenderCnt).toBe(0) // event still out of view
 
   resetCounts()
   $(window).simulate('resize')
   setTimeout(function() {
 
-    expectHeaderRendered(false)
-    expectGridRendered(true) // receives new coords
+    expect(dayLabelMountCnt).toBe(0)
+    expect(dayLabelRenderCnt).toBe(0)
+    expect(dayCellRenderCnt).toBe(0)
     expect(eventRenderCnt).toBe(0)
-
-    headerSpy.detach()
-    gridSpy.detach()
 
     done()
   }, 1) // more than windowResizeDelay
