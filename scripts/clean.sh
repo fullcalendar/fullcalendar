df9a6e34007f6c22c27ad8ed8b57e34197515c33
Adam Shaw <arshaw@users.noreply.github.com>
2020-05-12 23:06:29 -0400

    update clean script

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -6,17 +6,10 @@ cd "`dirname $0`/.." # always start in project root
 
 # make sure this list mirrors what's in .gitignore
 
-rm -rf tmp archives
+rm -rf archives
 
-rm -rf packages/*/*.js
-rm -rf packages/*/*.css
-rm -rf packages/*/*.map
-rm -rf packages/*/*.d.ts
-rm -rf packages/core/locales
-rm -rf packages/bundle/locales
+rm -rf packages/*/tsconfig.tsbuildinfo
+rm -rf packages/*/dist
 
-rm -rf packages-premium/*/*.js
-rm -rf packages-premium/*/*.css
-rm -rf packages-premium/*/*.map
-rm -rf packages-premium/*/*.d.ts
-rm -rf packages-premium/bundle/locales
+rm -rf packages-premium/*/tsconfig.tsbuildinfo
+rm -rf packages-premium/*/dist
