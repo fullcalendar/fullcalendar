d43a181014f6c3fc817b38e9d2ec9d342ecdf6aa
Adam Shaw <arshaw@users.noreply.github.com>
2020-05-18 02:25:59 -0400

    script rearranging

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -3,15 +3,14 @@
 set -e # immediately exit upon error
 cd "`dirname $0`/.." # start in project root
 
-yarn run clean
-
+# do react testing before the clean
 FULLCALENDAR_FORCE_REACT=1 yarn run build
 FULLCALENDAR_FORCE_REACT=1 yarn run test:ci
 
+yarn run clean
 yarn run build
-yarn run test:ci
-
 # yarn run lint # for v5!!!
+yarn run test:ci
 
 ./scripts/packages-contrib-ci.js
 ./scripts/example-projects-build.js
