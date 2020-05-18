fd4368f4541ea59cd06aa9625018e0a0f11122ea
Adam Shaw <arshaw@users.noreply.github.com>
2020-05-18 17:36:11 -0400

    add gcal to bundle

--- packages/bundle/src/main.ts
+++ packages/bundle/src/main.ts
@@ -5,6 +5,7 @@ export * from '@fullcalendar/daygrid'
 export * from '@fullcalendar/timegrid'
 export * from '@fullcalendar/list'
 export * from '@fullcalendar/bootstrap'
+export * from '@fullcalendar/google-calendar'
 
 import { globalPlugins } from '@fullcalendar/core'
 import interactionPlugin from '@fullcalendar/interaction'
@@ -12,11 +13,13 @@ import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import listPlugin from '@fullcalendar/list'
 import bootstrapPlugin from '@fullcalendar/bootstrap'
+import googleCalendarPlugin from '@fullcalendar/google-calendar'
 
 globalPlugins.push(
   interactionPlugin,
   dayGridPlugin,
   timeGridPlugin,
   listPlugin,
-  bootstrapPlugin
+  bootstrapPlugin,
+  googleCalendarPlugin
 )
