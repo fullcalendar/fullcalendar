439256b5561c662d615b186767bb51b0c3b4d0ae
Adam Shaw <arshaw@users.noreply.github.com>
2020-05-29 19:28:52 -0400

    use more compliant css nesting

--- postcss.config.js
+++ postcss.config.js
@@ -2,8 +2,7 @@ module.exports = {
   parser: require('postcss-comment'), // for "//" style comments
   plugins: [
     require('postcss-advanced-variables'),
-    require('postcss-nested'),
-    require('postcss-nested-ancestors'), // for & within nested ... needed?
+    require('postcss-nesting'),
     require('postcss-calc')
   ]
 }
