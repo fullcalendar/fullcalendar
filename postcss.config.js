29c82729a3d115f6705775df164402227f7651ef
Adam Shaw <arshaw@users.noreply.github.com>
2020-05-30 00:58:39 -0400

    start using postcss vars

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,13 @@ module.exports = {
   plugins: [
     require('postcss-advanced-variables'),
     require('postcss-nesting'),
+    require('postcss-css-variables')({
+      preserve: true // keep the var() expressions, with fallback
+    }),
     require('postcss-calc')
+
+    // TODO: remove empty blocks
+    // apparently it should automatically work with postcss-nesting, but doesn't seem to
+    // https://github.com/jonathantneal/postcss-nesting/issues/19
   ]
 }
