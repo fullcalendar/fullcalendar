5995f1da5dc9256804cda139101b50af5ee05cf8
viovoid <theviovoid@gmail.com>
2020-07-27 00:33:48 -0400

    Add Esperanto locale

---
+++ packages/core/src/locales/eo.ts
@@ -0,0 +1,22 @@
+import { LocaleInput } from '@fullcalendar/common'
+
+export default {
+  code: "eo",
+  week: {
+    dow: 1, // Monday is the first day of the week.
+    doy: 4  // The week that contains Jan 4th is the first week of the year.
+  },
+  buttonText: {
+    prev: "Antaŭa",
+    next: "Sekva",
+    today: "Hodiaŭ",
+    month: "Monato",
+    week: "Semajno",
+    day: "Tago",
+    list: "Tagordo"
+  },
+  weekText: "Sm",
+  allDayText: "Tuta tago",
+  moreLinkText: "pli",
+  noEventsText: "Neniuj eventoj por montri"
+} as LocaleInput
