fb86a4584793a7f71e474ecd803ea2e86c78ae18
Adam Shaw <arshaw@users.noreply.github.com>
2020-08-02 14:28:33 -0400

    misc

--- packages/core/src/vdom.ts
+++ packages/core/src/vdom.ts
@@ -1,2 +1,5 @@
 
+// this non-public package is inlined
+// don't let eslint complain that it's not referenced in package.json's deps
+// eslint-disable-next-line
 export * from '@fullcalendar/core-vdom'
