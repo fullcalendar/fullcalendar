0a1de1a26d7ab2fd76c5f13a6930d57cb064f02a
Adam Shaw <arshaw@users.noreply.github.com>
2020-08-12 15:25:29 -0400

    tests for touch dnd bug

---
+++ packages/__tests__/src/event-drag/touch.js
@@ -0,0 +1,34 @@
+import { DayGridViewWrapper } from '../lib/wrappers/DayGridViewWrapper'
+import { waitEventDrag } from '../lib/wrappers/interaction-util'
+
+describe('event touch dragging', function() {
+
+  // https://github.com/fullcalendar/fullcalendar/issues/5706
+  it('keeps event selected when initiated on custom element', function(done) {
+    let calendar = initCalendar({
+      initialDate: '2020-08-12',
+      editable: true,
+      longPressDelay: 100, // dragEventToDate waits 200. BAD
+      events: [
+        { title: 'event', start: '2020-08-12' }
+      ],
+      eventContent: { html: '<i>the text</i>' }
+    })
+    let gridWrapper = new DayGridViewWrapper(calendar).dayGrid
+    let eventEl = gridWrapper.getEventEls()[0]
+
+    let dragging = gridWrapper.dragEventToDate(
+      eventEl,
+      null, // don't specify start date. start drag on center of given element
+      '2020-08-13',
+      true,
+      eventEl.querySelector('i')
+    )
+
+    waitEventDrag(calendar, dragging).then((event) => {
+      expect(event.startStr).toBe('2020-08-13')
+      done()
+    })
+  })
+
+})
