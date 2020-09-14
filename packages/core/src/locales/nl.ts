125c84a21857410e8ab1445dc805a01ec3a85347
PieterDePauw <44448043+PieterDePauw@users.noreply.github.com>
2020-09-14 02:29:20 +0200

    Update nl.ts
    Change "Voorgaande" into "Vorige"

--- packages/core/src/locales/nl.ts
+++ packages/core/src/locales/nl.ts
@@ -7,7 +7,7 @@ export default {
     doy: 4  // The week that contains Jan 4th is the first week of the year.
   },
   buttonText: {
-    prev: "Voorgaand",
+    prev: "Vorige",
     next: "Volgende",
     today: "Vandaag",
     year: "Jaar",
