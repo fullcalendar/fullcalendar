1b2378bd4c9a7b4bee0da3007ed16bee87f6046a
Adam Shaw <arshaw@users.noreply.github.com>
2020-10-29 18:48:12 -0400

    patch to fix karma bug

--- scripts/packages-contrib-ci.js
+++ scripts/packages-contrib-ci.js
@@ -5,23 +5,17 @@ const globby = require('globby')
 const exec = require('./lib/shell')
 
 const PKGS_ROOT = path.resolve(__dirname, '../packages-contrib')
-const IS_CI = Boolean(process.env.CI)
 
 globby.sync('*', { cwd: PKGS_ROOT, onlyDirectories: true }).forEach(function(pkgDir) { // will match ONLY directories
   let pkgShortName = pkgDir.replace(/\/$/, '')
 
-  if (IS_CI && pkgShortName.match('angular')) {
-    console.log('Skipping @fullcalendar/angular when in a CI environment because of ChromeHeadless problems (fixed?)')
+  let { success } = exec.sync([ 'yarn', 'run', 'ci' ], {
+    cwd: path.join(PKGS_ROOT, pkgDir),
+    live: true
+  })
 
-  } else {
-    let { success } = exec.sync([ 'yarn', 'run', 'ci' ], {
-      cwd: path.join(PKGS_ROOT, pkgDir),
-      live: true
-    })
-
-    if (!success) {
-      console.warn(`Failed building package "${pkgShortName}"`)
-      process.exit(1)
-    }
+  if (!success) {
+    console.warn(`Failed building package "${pkgShortName}"`)
+    process.exit(1)
   }
 })
