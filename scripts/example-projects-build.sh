4947ab8b89c878c018afa53eda1c49eaa246c0a4
Adam Shaw <arshaw@users.noreply.github.com>
2020-11-02 17:28:46 -0500

    get things working

--- scripts/example-projects-build.sh
+++ scripts/example-projects-build.sh
@@ -9,37 +9,32 @@ for package_file in */package.json
 do
   project_name="$(dirname $package_file)"
 
+  echo "-------"
+  echo "PROJECT: $project_name"
+  echo "-------"
+
   case "$project_name" in
 
-    angular)
+    vue-typescript)
+      echo "SKIPPING $project_name FOR NOW" ;;
+    vue-vuex)
       echo "SKIPPING $project_name FOR NOW" ;;
     next)
       echo "SKIPPING $project_name FOR NOW" ;;
     nuxt)
       echo "SKIPPING $project_name FOR NOW" ;;
-    react)
-      echo "SKIPPING $project_name FOR NOW" ;;
-    react-mobx-typescript)
-      echo "SKIPPING $project_name FOR NOW" ;;
-    react-redux)
-      echo "SKIPPING $project_name FOR NOW" ;;
-    react-typescript)
-      echo "SKIPPING $project_name FOR NOW" ;;
-    vue)
-      echo "SKIPPING $project_name FOR NOW" ;;
-    vue-typescript)
-      echo "SKIPPING $project_name FOR NOW" ;;
-    vue-vuex)
-      echo "SKIPPING $project_name FOR NOW" ;;
     parcel)
       echo "SKIPPING $project_name FOR NOW" ;;
+
+    angular)
+      yarn pnpify --cwd angular yarn build
+      ;;
 
     *)
       cd "$project_name"
       yarn build
       cd -
-
-    # yarn pnpify --cwd angular yarn build
+      ;;
 
   esac
 
