cd359682fc92930ba75fd2ff230f96247fd06672
Adam Shaw <arshaw@users.noreply.github.com>
2020-11-04 20:59:08 -0500

    ci script improvements

---
+++ scripts/ci-all.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e # immediately exit upon error
+cd "`dirname $0`/.." # start in project root
+
+yarn run lint
+yarn run clean
+yarn run build
+yarn run lintBuilt
+yarn run archive
+yarn run contrib:ci
+yarn run ex:build
+yarn run test:ci
+FULLCALENDAR_FORCE_REACT=1 yarn run test:ci
