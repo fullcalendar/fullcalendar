d1e7e914447920aa933861e4c0a54bcf7b9f190a
benmc29 <45693623+benmc29@users.noreply.github.com>
2020-12-02 15:56:46 +0000

    Add Welsh Locale (cy.ts)

---
+++ packages/core/src/locales/cy.ts
@@ -0,0 +1,23 @@
+import { LocaleInput } from '@fullcalendar/common'
+
+export default {
+  code: 'cy',
+  week: {
+    dow: 1, // Monday is the first day of the week.
+    doy: 4, // The week that contains Jan 4th is the first week of the year.
+  },
+  buttonText: {
+    prev: 'Blaenorol',
+    next: 'Nesaf',
+    today: "Heddiw",
+    year: 'Blwyddyn',
+    month: 'Mis',
+    week: 'Wythnos',
+    day: 'Dydd',
+    list: 'Rhestr',
+  },
+  weekText: 'Wythnos',
+  allDayText: "Trwy'r dydd",
+  moreLinkText: 'Mwy',
+  noEventsText: 'Dim digwyddiadau',
+} as LocaleInput
