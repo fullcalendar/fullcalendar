531b33ed1224508e38b341a04e48640eb18f759d
Adam Shaw <arshaw@users.noreply.github.com>
2020-12-18 17:57:12 -0500

    refactor recent rrule stuff a bit, preferring RRuleSet, renaming some settings

--- packages/rrule/src/event-refiners.ts
+++ packages/rrule/src/event-refiners.ts
@@ -1,6 +1,7 @@
 import { createDuration, identity, Identity } from '@fullcalendar/common'
 
 type RRuleOptions = any // TODO: ask rrule maintainers to expose this
+  // NOTE: we added `exdate` and `exrule` to this
 
 export const RRULE_EVENT_REFINERS = {
   rrule: identity as Identity<RRuleOptions>,
--- packages/rrule/src/main.ts
+++ packages/rrule/src/main.ts
@@ -12,7 +12,7 @@ import { RRULE_EVENT_REFINERS } from './event-refiners'
 import './event-declare'
 
 interface RRuleParsed {
-  rrule: RRule
+  rruleSet: RRuleSet
   isTimeZoneSpecified: boolean
 }
 
@@ -24,7 +24,7 @@ let recurring: RecurringType<RRuleParsed> = {
 
       if (parsed) {
         return {
-          typeData: { rrule: parsed.rrule, isTimeZoneSpecified: parsed.isTimeZoneSpecified },
+          typeData: { rruleSet: parsed.rruleSet, isTimeZoneSpecified: parsed.isTimeZoneSpecified },
           allDayGuess: !parsed.isTimeSpecified,
           duration: refined.duration,
         }
@@ -38,7 +38,7 @@ let recurring: RecurringType<RRuleParsed> = {
     let dates: DateMarker[]
 
     if (parsed.isTimeZoneSpecified) {
-      dates = parsed.rrule.between(
+      dates = parsed.rruleSet.between(
         dateEnv.toDate(framingRange.start), // rrule lib will treat as UTC-zoned
         dateEnv.toDate(framingRange.end), // (same)
         true, // inclusive (will give extra events at start, see https://github.com/jakubroztocil/rrule/issues/84)
@@ -46,7 +46,7 @@ let recurring: RecurringType<RRuleParsed> = {
     } else {
       // when no timezone in given start/end, the rrule lib will assume UTC,
       // which is same as our DateMarkers. no need to manipulate
-      dates = parsed.rrule.between(
+      dates = parsed.rruleSet.between(
         framingRange.start,
         framingRange.end,
         true, // inclusive (will give extra events at start, see https://github.com/jakubroztocil/rrule/issues/84)
@@ -63,90 +63,108 @@ export default createPlugin({
 })
 
 function parseRRule(input, dateEnv: DateEnv) {
-  let isTimeSpecified = false
-  let isTimeZoneSpecified = false
-  let rrule
-
   if (typeof input === 'string') {
-    rrule = rrulestr(input)
-
-    let result = analyzeRRuleString(input)
-    isTimeSpecified = result.isTimeSpecified
-    isTimeZoneSpecified = result.isTimeZoneSpecified
+    return parseRRuleSetString(input)
   } else if (typeof input === 'object' && input) { // non-null object
-    let refined = { ...input } // copy
-    delete refined.exdates
-    delete refined.exrules
-
-    if (typeof refined.dtstart === 'string') {
-      let result = parseMarker(refined.dtstart)
-
-      // TODO: not DRY
-      if (result) {
-        isTimeSpecified = isTimeSpecified || !result.isTimeUnspecified
-        isTimeZoneSpecified = isTimeZoneSpecified || result.timeZoneOffset !== null
-        refined.dtstart = new Date(result.marker.valueOf() - (result.timeZoneOffset || 0) * 60 * 1000)
-      } else { // invalid
-        delete refined.dtstart // best idea?
-      }
-    }
-
-    if (typeof refined.until === 'string') {
-      let result = parseMarker(refined.until)
-
-      // TODO: not DRY
-      if (result) {
-        isTimeSpecified = isTimeSpecified || !result.isTimeUnspecified
-        isTimeZoneSpecified = isTimeZoneSpecified || result.timeZoneOffset !== null
-        refined.until = new Date(result.marker.valueOf() - (result.timeZoneOffset || 0) * 60 * 1000)
-      } else { // invalid
-        delete refined.until // best idea?
-      }
-    }
-
-    if (refined.freq != null) {
-      refined.freq = convertConstant(refined.freq)
-    }
-
-    if (refined.wkst != null) {
-      refined.wkst = convertConstant(refined.wkst)
-    } else {
-      refined.wkst = (dateEnv.weekDow - 1 + 7) % 7 // convert Sunday-first to Monday-first
-    }
-
-    if (refined.byweekday != null) {
-      refined.byweekday = convertConstants(refined.byweekday) // the plural version
-    }
-
-    rrule = new RRule(refined)
-
-    if (input.exdates || input.exrules) {
-      const exdates: [] = input.exdates instanceof Array ? input.exdates : []
-      const exrules: [] = input.exrules instanceof Array ? input.exrules : []
-
-      const rruleSet = new RRuleSet()
-      rruleSet.rrule(rrule)
-
-      for (let exrule of exrules) {
-        const rruleObj = parseRRule(exrule, dateEnv)
-        if (rruleObj && rruleObj.rrule) {
-          rruleSet.exrule(rruleObj.rrule)
-        }
-      }
-
-      for (let exdate of exdates) {
-        rruleSet.exdate(exdate)
-      }
-
-      rrule = rruleSet
-    }
-  }
-
-  if (rrule) {
-    return { rrule, isTimeSpecified, isTimeZoneSpecified }
+    return parseRRuleSetObject(input, dateEnv)
   }
 
   return null
+}
+
+function parseRRuleSetObject(input, dateEnv: DateEnv) {
+  let { rrule, isTimeSpecified, isTimeZoneSpecified } = parseRRuleObject(input, dateEnv)
+  let exdateInputs: any[] = [].concat(input.exdate || []) // convert to array
+  let exruleInputs: any[] = [].concat(input.exrule || []) // convert to array
+  let rruleSet = new RRuleSet()
+
+  rruleSet.rrule(rrule)
+
+  for (let exdateInput of exdateInputs) {
+    let exdateRes = parseMarker(exdateInput)
+
+    // TODO: not DRY
+    isTimeSpecified = isTimeSpecified || !exdateRes.isTimeUnspecified
+    isTimeZoneSpecified = isTimeZoneSpecified || exdateRes.timeZoneOffset !== null
+    rruleSet.exdate(
+      new Date(exdateRes.marker.valueOf() - (exdateRes.timeZoneOffset || 0) * 60 * 1000)
+    )
+  }
+
+  // TODO: exrule is deprecated. what to do? (https://icalendar.org/iCalendar-RFC-5545/a-3-deprecated-features.html)
+  for (let exruleInput of exruleInputs) {
+    let exruleRes = parseRRuleObject(exruleInput, dateEnv)
+
+    isTimeSpecified = isTimeSpecified || exruleRes.isTimeSpecified
+    isTimeZoneSpecified = isTimeZoneSpecified || exruleRes.isTimeZoneSpecified
+
+    rruleSet.exrule(exruleRes.rrule)
+  }
+
+  return { rruleSet, isTimeSpecified, isTimeZoneSpecified }
+}
+
+function parseRRuleObject(input, dateEnv: DateEnv) {
+  let isTimeSpecified = false
+  let isTimeZoneSpecified = false
+  let refined = { ...input } // copy
+
+  // TODO: weird to blacklist these here
+  delete refined.exdates
+  delete refined.exrules
+
+  if (typeof refined.dtstart === 'string') {
+    let result = parseMarker(refined.dtstart)
+
+    if (result) {
+      // TODO: not DRY
+      isTimeSpecified = isTimeSpecified || !result.isTimeUnspecified
+      isTimeZoneSpecified = isTimeZoneSpecified || result.timeZoneOffset !== null
+      refined.dtstart = new Date(result.marker.valueOf() - (result.timeZoneOffset || 0) * 60 * 1000)
+    } else { // invalid
+      delete refined.dtstart // best idea?
+    }
+  }
+
+  if (typeof refined.until === 'string') {
+    let result = parseMarker(refined.until)
+
+    if (result) {
+      // TODO: not DRY
+      isTimeSpecified = isTimeSpecified || !result.isTimeUnspecified
+      isTimeZoneSpecified = isTimeZoneSpecified || result.timeZoneOffset !== null
+      refined.until = new Date(result.marker.valueOf() - (result.timeZoneOffset || 0) * 60 * 1000)
+    } else { // invalid
+      delete refined.until // best idea?
+    }
+  }
+
+  if (refined.freq != null) {
+    refined.freq = convertConstant(refined.freq)
+  }
+
+  if (refined.wkst != null) {
+    refined.wkst = convertConstant(refined.wkst)
+  } else {
+    refined.wkst = (dateEnv.weekDow - 1 + 7) % 7 // convert Sunday-first to Monday-first
+  }
+
+  if (refined.byweekday != null) {
+    refined.byweekday = convertConstants(refined.byweekday) // the plural version
+  }
+
+  return {
+    rrule: new RRule(refined),
+    isTimeSpecified,
+    isTimeZoneSpecified,
+  }
+}
+
+function parseRRuleSetString(str) {
+  let rruleSet = rrulestr(str, { forceset: true }) as RRuleSet
+  let analysis = analyzeRRuleString(str)
+
+  return { rruleSet, ...analysis }
 }
 
 function analyzeRRuleString(str) {
