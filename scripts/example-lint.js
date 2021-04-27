c2cf5a27f072a7e001f04eebe82070ad3aeea715
Naguumo <ishbharal@gmail.com>
2021-04-27 10:25:24 -0500

    PR Fixes

--- scripts/example-lint.js
+++ scripts/example-lint.js
@@ -1,31 +1,31 @@
-const path = require("path");
-const exec = require("./lib/shell");
-const globby = require("globby");
+const path = require('path')
+const exec = require('./lib/shell')
+const globby = require('globby')
 
-const rootDir = path.resolve(__dirname, "..");
-const examplesDir = path.join(rootDir, "example-projects");
-const givenProjName = process.argv[2] || "all";
+const rootDir = path.resolve(__dirname, '..')
+const examplesDir = path.join(rootDir, 'example-projects')
+const givenProjName = process.argv[2] || 'all'
 
 const projNames =
-  givenProjName === "all"
-    ? globby.sync("*", { cwd: examplesDir, onlyDirectories: true })
-    : [givenProjName];
+  givenProjName === 'all'
+    ? globby.sync('*', { cwd: examplesDir, onlyDirectories: true })
+    : [givenProjName]
 
 projNames.forEach((projName) => {
-  const projDir = path.join(examplesDir, projName);
+  const projDir = path.join(examplesDir, projName)
 
-  console.log();
-  console.info("PROJECT:", projName);
-  console.log(projDir);
+  console.log()
+  console.info('PROJECT:', projName)
+  console.log(projDir)
 
-  const { success } = exec.sync(["yarn", "run", "lint"], {
+  const { success } = exec.sync(['yarn', 'run', 'lint'], {
     cwd: projDir,
     exitOnError: false,
     live: true,
-  });
+  })
 
   if (!success) {
-    console.log("Could not execute lint script, attempting generic linting");
+    console.log('Could not execute lint script, attempting generic linting')
 
     // Prettier is currently not part of any workspace, I will defer on adding it as a dependency
     // exec.sync(["yarn", "run", "prettier", "--write", "./src"], {
@@ -35,14 +35,14 @@ projNames.forEach((projName) => {
     // });
 
     exec.sync(
-      "yarn exec eslint src/ --fix-dry-run --config ../.eslintrc.json --ext .tsx,.ts,.jsx,.js",
+      'yarn exec eslint src/ --fix-dry-run --config ../.eslintrc.json --ext .tsx,.ts,.jsx,.js',
       {
         cwd: projDir,
         exitOnError: false,
         live: true,
       }
-    );
+    )
   }
 
-  console.log();
-});
+  console.log()
+})
--- scripts/example-run.js
+++ scripts/example-run.js
@@ -1,77 +1,72 @@
-const path = require("path");
-const exec = require("./lib/shell");
-const globby = require("globby");
+const path = require('path')
+const exec = require('./lib/shell')
+const globby = require('globby')
 
-const rootDir = path.resolve(__dirname, "..");
-const examplesDir = path.join(rootDir, "example-projects");
-const givenProjName = process.argv[2];
-const runCmd = process.argv[3];
+const rootDir = path.resolve(__dirname, '..')
+const examplesDir = path.join(rootDir, 'example-projects')
+const givenProjName = process.argv[2]
+const runCmd = process.argv[3]
 
 ///////////////////////////////////////////////////////
 // Project Settings
-const redirectProjects = [["parcel", "parcel-2"]];
-const disabledProjects = [
-  ["next", "This example is disabled till the next major release"],
-  ["next-scheduler", "This example is disabled till the next major release"],
-  [
-    "parcel-2",
-    "There is currently a bug in parcel bundler which prevents this from working",
-  ],
-];
-const pnpSimulatedProjects = ["angular"];
+const disabledProjects = {
+  next: 'This example is disabled till the next major release',
+  'next-scheduler': 'This example is disabled till the next major release',
+  parcel: 'This example is being transitioned to a newer version',
+  'parcel-2':
+    'There is currently a bug in parcel which prevents this from working',
+}
+const pnpSimulatedProjects = {
+  angular: 'Angular CLI does not support Yarn PnP',
+}
 ///////////////////////////////////////////////////////
 
 if (!givenProjName) {
-  console.error('Must specify an example-project name, or "all"');
-  process.exit(1);
+  console.error('Must specify an example-project name, or "all"')
+  process.exit(1)
 }
 
 if (!runCmd) {
-  console.error("Must specify a run command");
-  process.exit(1);
+  console.error('Must specify a run command')
+  process.exit(1)
 }
 
 const projNames =
-  givenProjName === "all"
-    ? globby.sync("*", { cwd: examplesDir, onlyDirectories: true })
-    : [givenProjName];
+  givenProjName === 'all'
+    ? globby.sync('*', { cwd: examplesDir, onlyDirectories: true })
+    : [givenProjName]
 
 projNames.forEach((projName) => {
-  // Rewrite projName to redirect directory
-  const redirect = redirectProjects.find(([val]) => val === projName);
-  if (redirect) {
-    console.info(`Redirecting '${redirect[0]}' to '${redirect[1]}' directory`);
-    projName = redirect[1];
-  }
-
   // Don't run disabled projects
-  const disabled = disabledProjects.find(([val]) => val === projName);
-  if (disabled) {
-    console.info(disabled[1]);
-    return;
+  if (Object.keys(disabledProjects).includes(projName)) {
+    const disabled = disabledProjects[projName]
+    console.info(disabled)
+    return
   }
 
-  const projDir = path.join(examplesDir, projName);
+  const projDir = path.join(examplesDir, projName)
 
-  console.log();
-  console.info("PROJECT:", projName);
-  console.log(projDir);
+  console.log()
+  console.info('PROJECT:', projName)
+  console.log(projDir)
 
   // Decide whether to simulate pnp or run normal yarn
-  let execCmd = [runCmd];
-  if (pnpSimulatedProjects.includes(projName)) {
-    console.log("Using PnP simulation");
-    execCmd = ["example:pnp", projName, runCmd];
+  if (Object.keys(pnpSimulatedProjects).includes(projName)) {
+    console.log('Using PnP simulation')
+    console.log()
+    exec.sync(['yarn', 'run', 'example:pnp', projName, runCmd], {
+      cwd: rootDir,
+      exitOnError: true,
+      live: true,
+    })
   } else {
-    console.log("Normal Yarn execution");
+    console.log('Normal Yarn execution')
+    console.log()
+    exec.sync(['yarn', 'run', runCmd], {
+      cwd: projDir,
+      exitOnError: true,
+      live: true,
+    })
   }
-
-  // Execute
-  console.log();
-  exec.sync(["yarn", "run", ...execCmd], {
-    cwd: projDir,
-    exitOnError: true,
-    live: true,
-  });
-  console.log();
-});
+  console.log()
+})
