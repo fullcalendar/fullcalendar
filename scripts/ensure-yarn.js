99ec84aef987c430e64ea3813f661ccf50a5fb63
Adam Shaw <arshaw@users.noreply.github.com>
2021-05-10 14:06:20 -0400

    make ensure-yarn work on windows

--- scripts/ensure-yarn.js
+++ scripts/ensure-yarn.js
@@ -1,4 +1,4 @@
-// https://stackoverflow.com/a/41233367/96342
-if (!/yarn\.js$/.test(process.env.npm_execpath)) {
+
+if (!/yarn(\.js)?$/.test(process.env.npm_execpath)) {
   throw new Error('Please ensure you are using Yarn (not NPM)')
 }
