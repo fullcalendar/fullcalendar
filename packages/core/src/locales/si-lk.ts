ba18e23ff03ef01ae7b2a9974358a616294e80ed
Adam Shaw <arshaw@users.noreply.github.com>
2021-10-12 18:28:45 -0400

    rename to si-lk

--- packages/core/src/locales/si-lk.ts
+++ packages/core/src/locales/si-lk.ts
@@ -1,7 +1,7 @@
 import { LocaleInput } from '@fullcalendar/common'
 
 export default {
-  code: 'lka',
+  code: 'si-lk',
   week: {
     dow: 1, // Monday is the first day of the week.
     doy: 4, // The week that contains Jan 4th is the first week of the year.
