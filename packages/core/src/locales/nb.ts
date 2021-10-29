5c5888538aa0d3f482bf4f5455d63ff7c63cdabc
Espen Antonsen <espen@inspired.no>
2021-10-29 14:07:58 +0200

    New locale options for Norwegian
    New options for locale. Ref https://github.com/fullcalendar/fullcalendar/blob/master/CHANGELOG.md#v5100-2021-10-13

--- packages/core/src/locales/nb.ts
+++ packages/core/src/locales/nb.ts
@@ -19,4 +19,14 @@ export default {
   allDayText: 'Hele dagen',
   moreLinkText: 'til',
   noEventsText: 'Ingen hendelser å vise',
+  buttonHints: {
+     prev: 'Forrige $0',
+     next: 'Neste $0',
+     today: 'Nåværende $0',
+   },
+   viewHint: '$0 visning',
+   navLinkHint: 'Gå til $0',
+   moreLinkHint(eventCnt: number) {
+     return `Vis ${eventCnt} flere hendelse${eventCnt === 1 ? '' : 'r'}`
+   },
 } as LocaleInput
