99c804a6e46fff0c5bed3d26df7f2336e282049a
fabien <fabien.winkler@outlook.fr>
2021-12-02 16:21:50 +0100

    fix : bootstrapFontAwesome doesn't work if don't use prototype

--- packages/bootstrap5/src/main.ts
+++ packages/bootstrap5/src/main.ts
@@ -2,37 +2,37 @@ import { Theme, createPlugin } from '@fullcalendar/common'
 import './main.css'
 
 export class Bootstrap5Theme extends Theme {
-  classes = {
-    root: 'fc-theme-bootstrap5', // TODO: compute this off of registered theme name
-    table: 'table-bordered', // don't attache the `table` class. we only want the borders, not any layout
-    tableCellShaded: 'table-active',
-    buttonGroup: 'btn-group',
-    button: 'btn btn-primary',
-    buttonActive: 'active',
-    popover: 'popover',
-    popoverHeader: 'popover-header',
-    popoverContent: 'popover-body',
-  }
-
-  baseIconClass = 'fa'
-  iconClasses = {
-    close: 'fa-times',
-    prev: 'fa-chevron-left',
-    next: 'fa-chevron-right',
-    prevYear: 'fa-angle-double-left',
-    nextYear: 'fa-angle-double-right',
-  }
-  rtlIconClasses = {
-    prev: 'fa-chevron-right',
-    next: 'fa-chevron-left',
-    prevYear: 'fa-angle-double-right',
-    nextYear: 'fa-angle-double-left',
-  }
-
-  iconOverrideOption = 'bootstrapFontAwesome' // TODO: make TS-friendly. move the option-processing into this plugin
-  iconOverrideCustomButtonOption = 'bootstrapFontAwesome'
-  iconOverridePrefix = 'fa-'
 }
+Bootstrap5Theme.prototype.classes = {
+  root: 'fc-theme-bootstrap5', // TODO: compute this off of registered theme name
+  table: 'table-bordered', // don't attache the `table` class. we only want the borders, not any layout
+  tableCellShaded: 'table-active',
+  buttonGroup: 'btn-group',
+  button: 'btn btn-primary',
+  buttonActive: 'active',
+  popover: 'popover',
+  popoverHeader: 'popover-header',
+  popoverContent: 'popover-body',
+}
+
+Bootstrap5Theme.prototype.baseIconClass = 'fa'
+Bootstrap5Theme.prototype.iconClasses = {
+  close: 'fa-times',
+  prev: 'fa-chevron-left',
+  next: 'fa-chevron-right',
+  prevYear: 'fa-angle-double-left',
+  nextYear: 'fa-angle-double-right',
+}
+Bootstrap5Theme.prototype.rtlIconClasses = {
+  prev: 'fa-chevron-right',
+  next: 'fa-chevron-left',
+  prevYear: 'fa-angle-double-right',
+  nextYear: 'fa-angle-double-left',
+}
+
+Bootstrap5Theme.prototype.iconOverrideOption = 'bootstrapFontAwesome' // TODO: make TS-friendly. move the option-processing into this plugin
+Bootstrap5Theme.prototype.iconOverrideCustomButtonOption = 'bootstrapFontAwesome'
+Bootstrap5Theme.prototype.iconOverridePrefix = 'fa-'
 
 const plugin = createPlugin({
   themeClasses: {
