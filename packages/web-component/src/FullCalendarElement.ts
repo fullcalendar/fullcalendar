1b31b593edb653ca2001540092da272148ac1626
Adam Shaw <arshaw@users.noreply.github.com>
2022-08-02 16:17:10 -0400

    read options from html attr

--- packages/web-component/src/FullCalendarElement.ts
+++ packages/web-component/src/FullCalendarElement.ts
@@ -1,24 +1,57 @@
-import { Calendar } from '@fullcalendar/core'
+import { Calendar, CalendarOptions } from '@fullcalendar/core'
 
 export class FullCalendarElement extends HTMLElement {
   _calendar: Calendar | null = null // TODO: make truly private
+  _options: CalendarOptions | null = null // TODO: same
 
   connectedCallback() {
+    const options = this._options || refineOptionsAttr(this.getAttribute('options')) || {}
+
     this.innerHTML = '<div></div>'
     let calendarEl = this.querySelector('div')
-    let calendar = new Calendar(calendarEl, {
-      headerToolbar: {
-        left: 'prev,next today',
-        center: 'title',
-        right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
-      },
-    })
+
+    let calendar = new Calendar(calendarEl, options)
     calendar.render()
+
     this._calendar = calendar
+    this._options = options
   }
 
   disconnectedCallback() {
     this._calendar.destroy()
+
     this._calendar = null
+    this._options = null
   }
+
+  attributeChangedCallback(name: string, oldVal: string, newVal: string): void {
+    if (name === 'options') {
+      const options = refineOptionsAttr(newVal)
+
+      if (options) {
+        if (this._calendar) {
+          this._calendar.resetOptions(options)
+        }
+        this._options = options
+      }
+    }
+  }
+
+  get options(): CalendarOptions {
+    return this._options
+  }
+
+  set options(newVal: CalendarOptions) {
+    if (this._calendar) {
+      this._calendar.resetOptions(newVal)
+    }
+    this._options = newVal
+  }
+}
+
+function refineOptionsAttr(optionsAttrStr: string | null): CalendarOptions | null {
+  if (optionsAttrStr) {
+    return JSON.parse(optionsAttrStr)
+  }
+  return null
 }
