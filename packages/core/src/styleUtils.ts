a30f9507758c005dbdd017cc8a85d3404a3584c7
Adam Shaw <arshaw@users.noreply.github.com>
2022-10-06 14:24:45 +0100

    semicolon rule

--- packages/core/src/styleUtils.ts
+++ packages/core/src/styleUtils.ts
@@ -1,15 +1,15 @@
 
 export function injectStyles(css: string): void {
-  if (!css || typeof document === 'undefined') { return; }
+  if (!css || typeof document === 'undefined') { return }
 
-  const head = document.head || document.getElementsByTagName('head')[0];
-  const style = document.createElement('style');
-  style.type = 'text/css';
-  head.appendChild(style);
+  const head = document.head || document.getElementsByTagName('head')[0]
+  const style = document.createElement('style')
+  style.type = 'text/css'
+  head.appendChild(style)
 
   if ((style as any).styleSheet) {
-    (style as any).styleSheet.cssText = css;
+    (style as any).styleSheet.cssText = css
   } else {
-    style.appendChild(document.createTextNode(css));
+    style.appendChild(document.createTextNode(css))
   }
 }
