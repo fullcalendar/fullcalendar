fullcalendar/fullcalendar-workspace

ridiulous changes to get moduleResolution:node16 to work

premium/tests/src/misc/eventAllow.ts
@@ -1,5 +1,5 @@
 import { waitEventDrag } from '@fullcalendar/standard-tests/src/lib/wrappers/interaction-util'
-import { ResourceTimelineViewWrapper } from '../lib/wrappers/ResourceTimelineViewWrapper'
+import { ResourceTimelineViewWrapper } from '../lib/wrappers/ResourceTimelineViewWrapper.js'
 
 describe('eventAllow', () => {
   pushOptions({

premium/tests/src/timeline/scrollTime.ts
@@ -1,4 +1,4 @@
-import { ResourceTimelineViewWrapper } from '../lib/wrappers/ResourceTimelineViewWrapper'
+import { ResourceTimelineViewWrapper } from '../lib/wrappers/ResourceTimelineViewWrapper.js'
 
 describe('scrollTime', () => {
   // https://github.com/fullcalendar/fullcalendar/issues/5351

premium/tests/src/misc/selectAllow.ts
@@ -1,5 +1,5 @@
 import { waitDateSelect } from '@fullcalendar/standard-tests/src/lib/wrappers/interaction-util'
-import { ResourceTimelineViewWrapper } from '../lib/wrappers/ResourceTimelineViewWrapper'
+import { ResourceTimelineViewWrapper } from '../lib/wrappers/ResourceTimelineViewWrapper.js'
 
 describe('selectAllow', () => {
   pushOptions({

premium/tests/src/timeline/slotLabelFormat.ts
@@ -1,4 +1,4 @@
-import { TimelineViewWrapper } from '../lib/wrappers/TimelineViewWrapper'
+import { TimelineViewWrapper } from '../lib/wrappers/TimelineViewWrapper.js'
 
 describe('slotLabelFormat', () => {
   pushOptions({

premium/tests/src/misc/mutateOptions.ts
@@ -1,7 +1,7 @@
 import { Calendar } from '@fullcalendar/core'
-import resourceTimelinePlugin from '@fullcalendar/resource-timeline'
+import { default as resourceTimelinePlugin } from '@fullcalendar/resource-timeline'
 import { CalendarWrapper } from '@fullcalendar/standard-tests/src/lib/wrappers/CalendarWrapper'
-import { ResourceTimelineViewWrapper } from '../lib/wrappers/ResourceTimelineViewWrapper'
+import { ResourceTimelineViewWrapper } from '../lib/wrappers/ResourceTimelineViewWrapper.js'
 
 function buildOptions() {
   return {

premium/tests/src/misc/scrollToTime.ts
@@ -1,4 +1,4 @@
-import { TimelineViewWrapper } from '../lib/wrappers/TimelineViewWrapper'
+import { TimelineViewWrapper } from '../lib/wrappers/TimelineViewWrapper.js'
 
 describe('scrollToTime method', () => {
   describe('when in timeline', () => {

fullcalendar/fullcalendar

tests/src/legacy/eventAllow.ts
@@ -1,6 +1,6 @@
-import { CalendarWrapper } from '../lib/wrappers/CalendarWrapper'
-import { TimeGridViewWrapper } from '../lib/wrappers/TimeGridViewWrapper'
-import { waitEventDrag } from '../lib/wrappers/interaction-util'
+import { CalendarWrapper } from '../lib/wrappers/CalendarWrapper.js'
+import { TimeGridViewWrapper } from '../lib/wrappers/TimeGridViewWrapper.js'
+import { waitEventDrag } from '../lib/wrappers/interaction-util.js'
 
 describe('eventAllow', () => {
   pushOptions({

tests/src/legacy/scrollTime.ts
@@ -1,4 +1,4 @@
-import { TimeGridViewWrapper } from '../lib/wrappers/TimeGridViewWrapper'
+import { TimeGridViewWrapper } from '../lib/wrappers/TimeGridViewWrapper.js'
 
 describe('scrollTime', () => {
   pushOptions({

tests/src/legacy/selectAllow.ts
@@ -1,5 +1,5 @@
-import { TimeGridViewWrapper } from '../lib/wrappers/TimeGridViewWrapper'
-import { waitDateSelect } from '../lib/wrappers/interaction-util'
+import { TimeGridViewWrapper } from '../lib/wrappers/TimeGridViewWrapper.js'
+import { waitDateSelect } from '../lib/wrappers/interaction-util.js'
 
 describe('selectAllow', () => {
   pushOptions({

tests/src/legacy/slotLabelFormat.ts
@@ -1,5 +1,5 @@
-import enGbLocale from '@fullcalendar/core/locales/en-gb'
-import { TimeGridViewWrapper } from '../lib/wrappers/TimeGridViewWrapper'
+import { default as enGbLocale } from '@fullcalendar/core/locales/en-gb'
+import { TimeGridViewWrapper } from '../lib/wrappers/TimeGridViewWrapper.js'
 
 describe('slotLabelFormat', () => {
   pushOptions({

tests/src/performance/mutateOptions.ts
@@ -1,7 +1,7 @@
 import { Calendar } from '@fullcalendar/core'
-import timeGridPlugin from '@fullcalendar/timegrid'
-import { TimeGridViewWrapper } from '../lib/wrappers/TimeGridViewWrapper'
-import { CalendarWrapper } from '../lib/wrappers/CalendarWrapper'
+import { default as timeGridPlugin } from '@fullcalendar/timegrid'
+import { TimeGridViewWrapper } from '../lib/wrappers/TimeGridViewWrapper.js'
+import { CalendarWrapper } from '../lib/wrappers/CalendarWrapper.js'
 
 function buildOptions() {
   return {

tests/src/view-render/scrollToTime.ts
@@ -1,4 +1,4 @@
-import { TimeGridViewWrapper } from '../lib/wrappers/TimeGridViewWrapper'
+import { TimeGridViewWrapper } from '../lib/wrappers/TimeGridViewWrapper.js'
 
 describe('scrollToTime method', () => {
   it('accepts a object duration input', () => {