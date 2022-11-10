1406dbb3aded23a829f9ce984f4092a2b66baff2
Adam Shaw <arshaw@users.noreply.github.com>
2022-11-10 15:47:47 -0500

    use ViewApi in render-hook-misc

--- packages/core/src/render-hook-misc.ts
+++ packages/core/src/render-hook-misc.ts
@@ -1,12 +1,12 @@
 import { DateMeta } from './component/date-rendering.js'
 import { Duration } from './datelib/duration.js'
-import { ViewImpl } from './api/ViewImpl.js'
+import { ViewApi } from './api/ViewApi.js'
 import { MountArg } from './common/render-hook.js'
 
 export interface SlotLaneContentArg extends Partial<DateMeta> { // TODO: move?
   time?: Duration
   date?: Date
-  view: ViewImpl
+  view: ViewApi
   // this interface is for date-specific slots AND time-general slots. make an OR?
 }
 export type SlotLaneMountArg = MountArg<SlotLaneContentArg>
@@ -15,20 +15,20 @@ export interface SlotLabelContentArg { // TODO: move?
   level: number
   time: Duration
   date: Date
-  view: ViewImpl
+  view: ViewApi
   text: string
 }
 export type SlotLabelMountArg = MountArg<SlotLabelContentArg>
 
 export interface AllDayContentArg {
   text: string
-  view: ViewImpl
+  view: ViewApi
 }
 export type AllDayMountArg = MountArg<AllDayContentArg>
 
 export interface DayHeaderContentArg extends DateMeta {
   date: Date
-  view: ViewImpl
+  view: ViewApi
   text: string
   [otherProp: string]: any
 }
