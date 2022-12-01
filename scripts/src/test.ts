391518c644cfe8cff983d5aef8000b85e41774f9
Adam Shaw <arshaw@users.noreply.github.com>
2022-11-30 20:10:02 -0500

    fix test script. --other arg

--- scripts/src/test.ts
+++ scripts/src/test.ts
@@ -5,6 +5,7 @@ import { execLive } from './utils/exec.js'
 export default async function(this: ScriptContext, ...args: string[]) {
   const isDev = args.includes('--dev')
   const isAll = args.includes('--all')
+  const isOther = args.includes('--other')
   const { monorepoStruct } = this
   const { pkgDirToJson } = monorepoStruct
   const promises: Promise<any>[] = []
@@ -12,14 +13,14 @@ export default async function(this: ScriptContext, ...args: string[]) {
   for (const pkgDir in pkgDirToJson) {
     const pkgJson = pkgDirToJson[pkgDir]
 
-    if (isAll || pkgJson.karmaConfig) {
-      if (isDev) {
-        if (pkgJson.karmaConfig) {
-          promises.push(
-            execLive(['pnpm', 'run', 'test:dev'], { cwd: pkgDir }),
-          )
-        }
-      } else {
+    if (
+      isAll ||
+      (isOther && !pkgJson.karmaConfig) ||
+      (!isOther && pkgJson.karmaConfig)
+    ) {
+      const subcommand = isDev ? 'test:dev' : 'test'
+
+      if (pkgJson.scripts?.[subcommand]) {
         console.log()
         console.log(chalk.green(pkgJson.name))
         console.log()
