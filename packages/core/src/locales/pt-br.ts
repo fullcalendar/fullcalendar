3fd08d8b6abe4e390995b5f6d27b132430aed0e6
Wesley Calixto dos Reis <wesley.c.reis@gmail.com>
2022-12-22 22:47:31 -0300

    Update pt-br Locale
    Some buttons and texts were not translated into Brazilian Portuguese. Please merge into the project.

--- packages/core/src/locales/pt-br.ts
+++ packages/core/src/locales/pt-br.ts
@@ -11,10 +11,32 @@ export default {
     day: 'Dia',
     list: 'Lista',
   },
+  buttonHints: {
+    prev: '$0 anterior',
+    next: '$0 seguinte',
+    today(buttonText) {
+      return (buttonText === 'Dia') ? 'Hoje' :
+        ((buttonText === 'Semana') ? 'Esta' : 'Este') + ' ' + buttonText.toLocaleLowerCase()
+    },
+  },
+  viewHint(buttonText) {
+    return 'Visualizar ' + (buttonText === 'Semana' ? 'a' : 'o') + ' ' + buttonText.toLocaleLowerCase()
+  },  
   weekText: 'Sm',
+  weekTextLong: 'Semana',
+  moreLinkText: 'mais',  
   allDayText: 'dia inteiro',
+  moreLinkHint(eventCnt) {
+    return `Mostrar mais ${eventCnt} eventos`
+  },
   moreLinkText(n) {
     return 'mais +' + n
   },
   noEventsText: 'Não há eventos para mostrar',
+  navLinkHint: 'Ir para $0',
+  closeHint: 'Fechar',
+  timeHint: 'A hora',
+  eventHint: 'Evento',  
+  
+  
 } as LocaleInput
