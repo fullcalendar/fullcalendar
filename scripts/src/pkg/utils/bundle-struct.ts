a11d83dda9904b476672c8ef26687d20ed2fe841
Adam Shaw <arshaw@users.noreply.github.com>
2023-01-30 14:51:11 -0500

    throw error when entrypoint doesnt exist

--- scripts/src/pkg/utils/bundle-struct.ts
+++ scripts/src/pkg/utils/bundle-struct.ts
@@ -86,6 +86,10 @@ async function unglobEntryStructMap(
   const srcDir = joinPaths(pkgDir, 'src')
   const srcPaths = await globby(massagedGlob, { cwd: srcDir })
 
+  if (!srcPaths.length) {
+    throw new Error(`Glob '${entryGlob}' does not exist in package '${pkgDir}'`)
+  }
+
   for (const srcPath of srcPaths) {
     for (const srcExtension of srcExtensions) {
       if (srcPath.endsWith(srcExtension)) {
