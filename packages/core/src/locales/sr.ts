46255e7c357fd4566f33ae3f539ea0af9fd5085c
Vladimir Milovanović <vladimir@milovanovic.me>
2025-05-12 17:00:29 +0200

    Update sr.ts code to proper latn value
    Fixes issue 5590

--- packages/core/src/locales/sr.ts
+++ packages/core/src/locales/sr.ts
@@ -1,7 +1,7 @@
 import { LocaleInput } from '../index.js'
 
 export default {
-  code: 'sr',
+  code: 'sr-latn-rs',
   week: {
     dow: 1, // Monday is the first day of the week.
     doy: 7, // The week that contains Jan 1st is the first week of the year.
