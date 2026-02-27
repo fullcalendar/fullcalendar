b7f1d682c9f605d8a7e0efa4540ec6dc202ecfbd
Sungjoon Moon <sumoon@seoulsaram.org>
2026-02-28 01:41:57 +0900

    Improve Korean locale translation

--- packages/core/src/locales/ko.ts
+++ packages/core/src/locales/ko.ts
@@ -3,17 +3,17 @@ import { LocaleInput } from '../index.js'
 export default {
   code: 'ko',
   buttonText: {
-    prev: '이전달',
-    next: '다음달',
+    prev: '이전',
+    next: '다음',
     today: '오늘',
-    year: '년도',
+    year: '연도',
     month: '월',
     week: '주',
     day: '일',
-    list: '일정목록',
+    list: '일정',
   },
   weekText: '주',
   allDayText: '종일',
-  moreLinkText: '개',
+  moreLinkText: '개 더보기',
   noEventsText: '일정이 없습니다',
 } as LocaleInput
